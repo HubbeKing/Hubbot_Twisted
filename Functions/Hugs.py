8f1833a12572a28132e26aeed481224114a9decc
Hubbe King <hubbe128@gmail.com>
2013-12-02 19:18:15 +0200

    Start work on the HugTracker

---
+++ Functions/Hugs.py
@@ -0,0 +1,9 @@
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse, ResponseType
+from Function import Function
+import GlobalVars
+
+class Instantiate(Function):
+	Help = "hugs [nick] -- How many hugs has this person given and received?"
+	
+	
