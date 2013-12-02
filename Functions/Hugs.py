2442223db162e95defc1700275b9286446f6a4f2
Hubbe King <hubbe128@gmail.com>
2013-12-02 19:35:57 +0200

    Skeleton for hugtracker

--- Functions/Hugs.py
+++ Functions/Hugs.py
@@ -6,4 +6,8 @@ import GlobalVars
 class Instantiate(Function):
 	Help = "hugs [nick] -- How many hugs has this person given and received?"
 	
-	
+	if message.Type == "ACTION":
+		#track hugs
+	elif message.Type == "PRIVMSG":
+		if message.Command == "hugs":
+			#return hug amount
