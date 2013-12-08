23b37f91eeb8e3557885989494f9b95ba74dd038
HubbeKing <hubbe128@gmail.com>
2013-12-08 19:45:20 +0200

    Combine the new sqlite3 database scripts into one

--- newDB.py
+++ newDB.py
@@ -1,13 +1,16 @@
 import sqlite3
 
 hugs = ["hubbeking",0,0]
+line = "Symphony is a cyborg."
 
 conn = sqlite3.connect("data/data.db")
 c = conn.cursor()
 
 c.execute("CREATE TABLE hugs (nick text, given int, received int)")
+c.execute("CREATE TABLE headcanon (canon text)")
 
 c.execute("INSERT INTO hugs VALUES (?, ?, ?)", (hugs[0], hugs[1], hugs[2]))
+c.execute("INSERT INTO headcanon VALUES (?)", (line,))
 
 conn.commit()
 conn.close()
