c7415143f2789f141e1414ef4080a1e1e6075fdf
HubbeKing <hubbe128@gmail.com>
2013-12-08 21:24:33 +0200

    Actually get basic multiserver support going

--- MultiServer.py
+++ MultiServer.py
@@ -1,8 +1,17 @@
+from multiprocessing import Process
+import subprocess
 import GlobalVars
 
-for (server,channels) in GlobalVars.connections.items():
-    args = ["hubbot.py"]
+
+def botInstance(server, channels):
+    args = ["python", "hubbebot.py"]
     args.append(server)
     for chan in channels:
         args.append(chan)
     subprocess.call(args)
+
+
+if __name__ == "__main__":
+    for (server,channels) in GlobalVars.connections.items():
+        p = Process(target=botInstance, args=(server, channels))
+        p.start()
