21718e3bcc7daa3929906e9c9685fb7dbb7b5f44
Hubbe King <hubbe128@gmail.com>
2014-01-27 18:45:11 +0200

    Less channels for more better

--- GlobalVars.py
+++ GlobalVars.py
@@ -3,7 +3,7 @@ import os
 functions = {}
 
 CurrentNick = "Hubbot"
-connections = {"irc.desertbus.org":["#desertbus", "#unmoderated", "#help", "#games"], "irc.applejack.me":["#survivors"]}
+connections = {"irc.desertbus.org":["#desertbus", "#unmoderated"], "irc.applejack.me":["#survivors"]}
 
 port = 6667
 
