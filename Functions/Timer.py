98767ad6936d2bcff380c928e37c1a6603e454f0
Hubbe King <hubbe128@gmail.com>
2014-02-26 12:45:45 +0200

    Make a timer function?

---
+++ Functions/Timer.py
@@ -0,0 +1,19 @@
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse, ResponseType
+from Function import Function
+import GlobalVars
+
+class Instantiate(Function):
+    Help = "timer <time> - starts a countdown timer and notifies you when time's up."
+
+    def GetResponse(self, HubbeBot, message):
+        if message.Type != "PRIVMSG":
+            return
+        if len(message.ParameterList) != 1:
+            return IRCResponse(ResponseType.Say, "Please use only 1 argument.", message.ReplyTo)
+        
+        delay = float(ParameterList[0])
+        HubbeBot.reactor.callLater(self.notifyUser(message):
+
+    def notifyUser(message):
+        HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Time is up!", message.ReplyTo))
