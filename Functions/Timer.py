062655acfee614e581d407ce514ca8fcd696586d
Hubbe King <hubbe128@gmail.com>
2014-02-26 12:48:40 +0200

    Ohgod why am I so dumb

--- Functions/Timer.py
+++ Functions/Timer.py
@@ -9,11 +9,11 @@ class Instantiate(Function):
     def GetResponse(self, HubbeBot, message):
         if message.Type != "PRIVMSG":
             return
-        if len(message.ParameterList) != 1:
-            return IRCResponse(ResponseType.Say, "Please use only 1 argument.", message.ReplyTo)
-        
-        delay = float(ParameterList[0])
-        HubbeBot.reactor.callLater(self.notifyUser(message):
+        if message.Command == "timer":
+            if len(message.ParameterList) != 1:
+                return IRCResponse(ResponseType.Say, "Please use only 1 argument.", message.ReplyTo)
+            delay = float(ParameterList[0])
+            HubbeBot.reactor.callLater(self.notifyUser(message))
 
     def notifyUser(message):
         HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Time is up!", message.ReplyTo))
