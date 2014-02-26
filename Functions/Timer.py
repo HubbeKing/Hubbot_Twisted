371e19ce1190ebf51d9b35feab38dd28727a7453
Hubbe King <hubbe128@gmail.com>
2014-02-26 13:31:42 +0200

    More things for more better.

--- Functions/Timer.py
+++ Functions/Timer.py
@@ -13,10 +13,13 @@ class Instantiate(Function):
         if message.Command == "timer":
             if len(message.ParameterList) != 1:
                 return IRCResponse(ResponseType.Say, "Please use only 1 argument.", message.ReplyTo)
-            delay = float(message.ParameterList[0])
-            reactor.callLater(delay, self.notifyUser, HubbeBot, message)
+            try:
+                delay = float(message.ParameterList[0])
+            except:
+                return IRCResponse(ResponseType.Say, "That doesn't look like a number to me...", message.ReplyTo)
+            reactor.callLater(delay, self.notifyUser, HubbeBot, delay, message)
             return IRCResponse(ResponseType.Say, message.User.Name + ": A timer has been started!", message.ReplyTo)
             
             
-    def notifyUser(self, HubbeBot, message):
-        HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Time is up!", message.ReplyTo))
+    def notifyUser(self, HubbeBot, delay, message):
+        HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Your " + str(int(delay)) + " second timer is up!" , message.ReplyTo))
