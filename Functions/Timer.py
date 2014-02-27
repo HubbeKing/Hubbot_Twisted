8545a3fbf081d2abecb211bc6b56a44576774314
Hubbe King <hubbe128@gmail.com>
2014-02-27 11:18:22 +0200

    Better handling of different time-formats

--- Functions/Timer.py
+++ Functions/Timer.py
@@ -13,24 +13,31 @@ class Instantiate(Function):
         if message.Type != "PRIVMSG":
             return
         if message.Command == "timer":
+            flag = False
             if len(message.ParameterList) != 1:
                 return IRCResponse(ResponseType.Say, "Please use only 1 argument.", message.ReplyTo)
             try:
-                delay = timeparse(message.ParameterList[0])
-                if delay > (60*60*24*365):
-                    return IRCResponse(ResponseType.Say, "Do you really need a timer that long?", message.ReplyTo)
+                delay = int(message.ParameterList[0])
+                flag = True
             except:
-                return IRCResponse(ResponseType.Say, "I don't understand that time...", message.ReplyTo)
-
+                delay = timeparse(message.ParameterList[0])
             if delay <= 0 or delay == None:
                 return IRCResponse(ResponseType.Say, "T don't think I understand that...", message.ReplyTo)
+            elif delay > (60*60*24*365):
+                return IRCResponse(ResponseType.Say, "Do you really need a timer that long?", message.ReplyTo)
             elif delay < 1:
                 return IRCResponse(ResponseType.Say, "Less than a second? Really?", message.ReplyTo)
 
             else:
-                reactor.callLater(delay, self.notifyUser, HubbeBot, message)
-                return IRCResponse(ResponseType.Say, message.User.Name + ": A " + message.ParameterList[0] + " timer has been started!", message.ReplyTo)
+                reactor.callLater(delay, self.notifyUser, HubbeBot, flag, message)
+                if flag:
+                    return IRCResponse(ResponseType.Say, message.User.Name + ": A " + message.ParameterList[0] + " second timer has been started!", message.ReplyTo)
+                else: 
+                    return IRCResponse(ResponseType.Say, message.User.Name + ": A " + message.ParameterList[0] + " timer has been started!", message.ReplyTo)
+                    
             
-            
-    def notifyUser(self, HubbeBot, message):
-        HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Your " + message.ParameterList[0] + " timer is up!" , message.ReplyTo))
+    def notifyUser(self, HubbeBot, flag, message):
+        if flag:
+            HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Your " + message.ParameterList[0] + " second timer is up!" , message.ReplyTo))
+        else:
+            HubbeBot.sendResponse(IRCResponse(ResponseType.Say, message.User.Name + ": Your " + message.ParameterList[0] + " timer is up!" , message.ReplyTo))
