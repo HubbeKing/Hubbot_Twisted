42720b14f376669f7eb992bb2bf8816097761338
Hubbe King <hubbe128@gmail.com>
2014-02-27 16:28:39 +0200

    RUSTLEEEEE

--- Functions/IdentCheck.py
+++ Functions/IdentCheck.py
@@ -4,7 +4,7 @@ from Function import Function
 import random
 
 class Instantiate(Function):
-    Help = "Reveal the secret inside... WHAT ARE YOU REALLY?"
+    Help = "rustle <rustlee> - There's no need to be upset. REVEAL THE SECRET INSIDE."
 
     def GetResponse(self, HubbeBot, message):
         if message.Type != "PRIVMSG":
@@ -13,6 +13,8 @@ class Instantiate(Function):
             roll = random.randint(1,20)
             if (message.User.Name == "BillTheCat"):
                 return IRCResponse(ResponseType.Say, "Uhm... Hi?", message.ReplyTo)
+            if message.User.Name == "Caitiri":
+                return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "KITTEH!"', message.ReplyTo)
             elif (roll == 1):
                 reroll = random.randint(1,20)
                 if (reroll == 20):
@@ -45,3 +47,17 @@ class Instantiate(Function):
                 return IRCResponse(ResponseType.Say, message.User.Name + " is a DINOSAUR.", message.ReplyTo)
             else:
                 return IRCResponse(ResponseType.Say, message.User.Name + " is a CRITICAL DINOSAUR!", message.ReplyTo)
+
+        if message.Command == "rustle":
+            if len(message.ParameterList) != 1:
+                return IRCResponse(ResponseType.Say, "Rustle who?", message.ReplyTo)
+            else:
+                roll = random.randint(1,20)
+                if roll == 1:
+                    return IRCResponse(ResponseType.Say, message.User.Name + " has rustled their own jimmies in their critical failure!", message.ReplyTo)
+                elif  (roll > 1) and (roll < 12):
+                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmes status: unrustled", message.ReplyTo)
+                elif (roll > 11) and (roll < 20):
+                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmes status: rustled", message.ReplyTo)
+                else:
+                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmes status: CRITICAL RUSTLE", message.ReplyTo)
