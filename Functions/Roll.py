0393f9cb39a43fbe96d9fd89335aa189afb0b6e5
Hubbe King <hubbe128@gmail.com>
2014-03-11 13:08:34 +0200

    Such debug
    Very mistakes

--- Functions/Roll.py
+++ Functions/Roll.py
@@ -10,72 +10,83 @@ class Instantiate(Function):
         if message.Type != "PRIVMSG":
             return
         if message.Command == "roll":
-            verbose = False
-            negIndex = posIndex = False
-            if len(message.ParameterList) < 1:
-                return IRCResponse(ResponseType.Say, "Roll what?", message.ReplyTo)
-            if message.ParameterList[0].find("d")==-1:
-                return IRCResponse(ResponseType.Say, "I don't understand that expression...", message.ReplyTo)
-            else:
-                dIndex = message.ParameterList[0].index("d")
-            if message.ParameterList[0].find("v")==-1 or message.ParameterList[1].find("v"):
+            if len(message.ParameterList) > 1 and (message.ParameterList[0].find("v")==-1 or message.ParameterList[1].find("v")):
                 verbose = False
+                paramList = message.ParameterList
+            elif message.ParameterList[0].find("v")==-1 and len(message.ParameterList) == 1:
+                verbose = False
+                paramList = message.ParameterList
             else:
                 verbose = True
-            if message.ParameterList[0].find("+")==-1:
-                if message.ParameterList[0].find("-")==-1:
-                    if len(message.ParameterList) != 2:
+                paramList = []
+                for item in message.ParameterList:
+                    paramList.append(item.replace("v",""))
+            negIndex = posIndex = False
+            
+            if len(paramList) < 1:
+                return IRCResponse(ResponseType.Say, "Roll what?", message.ReplyTo)
+            if paramList[0].find("d")==-1:
+                return IRCResponse(ResponseType.Say, "I don't understand that expression...", message.ReplyTo)
+            else:
+                dIndex = paramList[0].index("d")
+            
+            if paramList[0].find("+")==-1:
+                if paramList[0].find("-")==-1:
+                    if len(paramList) != 2:
                         modifier = 0
                     else:
                         # interpret ParameterList[1] as modifier
-                        modifier = int(ParameterList[1])
+                        try:
+                            modifier = int(paramList[1])
+                        except:
+                            return IRCResponse(ResponseType.Say, "I don't understand that.41", message.ReplyTo)
                 else:
                     # find - modifier in ParameterList[0]
-                    negIndex = message.ParameterList[0].index("-")
+                    negIndex = paramList[0].index("-")
                     try:
-                        modifier = int(message.ParameterList[0][negIndex+1:])
+                        modifier = int(paramList[0][negIndex+1:])
                         modifier = -modifier
                     except:
-                        return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                        return IRCResponse(ResponseType.Say, "I don't understand that.49", message.ReplyTo)
             else:
                 # find + modifier in ParameterList[0]
-                posIndex = message.ParameterList[0].index("+")
+                posIndex = paramList[0].index("+")
                 try:
-                    modifier = int(message.ParameterList[0][posIndex+1:])
+                    modifier = int(paramList[0][posIndex+1:])
                 except:
-                    return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, "I don't understand that.56", message.ReplyTo)
 
             if negIndex == False and posIndex == False:
                 # no modifier in ParameterList[0]
+                #try:
+                sides = int(paramList[0][dIndex+1:])
+                #except:
+                #    return IRCResponse(ResponseType.Say, "I don't understand that.63", message.ReplyTo)
                 try:
-                    sides = int(message.ParameterList[0][dIndex+1:])
+                    numberOfDice = int(paramList[0][0:dIndex])
                 except:
-                    return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
-                try:
-                    numberOfDice = int(message.ParameterList[0][0:dIndex])
-                except:
-                    return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, "I don't understand that.67", message.ReplyTo)
             else:
-                if negIndex == False:
+                if negIndex != False:
                     # negative sign in ParameterList[0]
                     try:
-                        sides = int(message.ParameterList[0][dIndex+1:negIndex])
+                        sides = int(paramList[0][dIndex+1:negIndex])
                     except:
-                        return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                        return IRCResponse(ResponseType.Say, "I don't understand that.74", message.ReplyTo)
                     try:
-                        numberOfDice = int(message.ParameterList[0][0:dIndex])
+                        numberOfDice = int(paramList[0][0:dIndex])
                     except:
-                        return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                        return IRCResponse(ResponseType.Say, "I don't understand that.78", message.ReplyTo)
                 else:
                     # positive sign in ParameterList[0]
                     try:
-                        sides = int(message.ParameterList[0][dIndex+1:posIndex])
+                        sides = int(paramList[0][dIndex+1:posIndex])
                     except:
-                        return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                        return IRCResponse(ResponseType.Say, "I don't understand that.84", message.ReplyTo)
                     try:
-                        numberOfDice = int(message.ParameterList[0][0:dIndex])
+                        numberOfDice = int(paramList[0][0:dIndex])
                     except:
-                        return IRCResponse(ResponseType.Say, "I don't understand that.", message.ReplyTo)
+                        return IRCResponse(ResponseType.Say, "I don't understand that.88", message.ReplyTo)
                     
             if numberOfDice < 33 and sides < 201:
                 results = []
@@ -86,7 +97,7 @@ class Instantiate(Function):
 
             if modifier > 100:
                 return IRCResponse(ResponseType.Say, "That modifier is too big, silly!", message.ReplyTo)
-            if modifier < 100:
+            if modifier < -100:
                 return IRCResponse(ResponseType.Say, "That modifier is too big, silly!", message.ReplyTo)
 
             if modifier < 0:
