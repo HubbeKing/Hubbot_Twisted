a977a150c9d96282a2e82a1d2f3d6671353d8c5e
HubbeKing <hubbe128@gmail.com>
2014-03-11 10:34:40 +0200

    Verbose and modifier support...
    Now I just gotta do the actual expression interpreting and dice
    rolling...

--- Functions/Roll.py
+++ Functions/Roll.py
@@ -4,16 +4,34 @@ from Function import Function
 import random
 
 class Instantiate(Function):
-    Help = "roll <dice> - Roll up some polyhedral dice! Ex. roll 1d20"
+    Help = "roll <dice> - Roll up some polyhedral dice! Ex: XdY+Zv - roll X Y-sided dice, with a +Z modifier and verbose output"
 
     def GetResponse(self, HubbeBot, message):
         if message.Type != "PRIVMSG":
             return
         if message.Command == "roll":
+            verbose = False
             if len(message.ParameterList) < 1:
                 return IRCResponse(ResponseType.Say, "Roll what?", message.ReplyTo)
             if message.ParameterList[0].find("d")==-1:
                 return IRCResponse(ResponseType.Say, "I don't understand that expression...", message.ReplyTo)
             else:
                 dIndex = message.ParameterList[0].index("d")
-        
+            if message.ParameterList[0].find("v")==-1 or message.ParameterList[1].find("v"):
+                continue
+            else:
+                verbose = True
+            if message.ParameterList[0].find("+")==-1:
+                if message.ParameterList[0].find("-")==-1:
+                    if len(message.ParameterList) != 2:
+                        # no modifier
+                    else:
+                        # interpret ParameterList[1] as modifier, store somewhere?
+                else:
+                    # find - modifier in ParameterList[0]
+            else:
+                # find + modifier in ParameterList[0]
+
+            # use dIndex to interpret dice expression
+            # roll them dice
+            # if verbose, output entire list and sum, otherwise just sum
