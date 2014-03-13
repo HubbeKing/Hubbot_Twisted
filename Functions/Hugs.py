1fb4013f9803f7c1fd35eec5efc2b80b4bb33c6c
Hubbe King <hubbe128@gmail.com>
2014-03-13 20:52:14 +0200

    In today's episode of GitHub, I am stupid.

--- Functions/Hugs.py
+++ Functions/Hugs.py
@@ -102,6 +102,6 @@ class Instantiate(Function):
 					else:
 						matchedNicksString = matchedNicksString + name + ", "
 						numberOfMatches += 1
-				if len(matchedNicksString) > 50:
+				if len(matches) > 30:
                                         matchedNicksString = "Matches found: LOTS."
 				return IRCResponse(ResponseType.Say, matchedNicksString, message.ReplyTo), IRCResponse(ResponseType.Say, HugString, message.ReplyTo)
