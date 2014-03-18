7afd369a41cffd038a2ad978979e1bec716d9998
HubbeKing <hubbe128@gmail.com>
2014-03-18 11:31:59 +0200

    Separate Rustle and IdentCheck - fixes #6

--- Functions/RoBoBo.py
+++ Functions/RoBoBo.py
@@ -10,9 +10,9 @@ class Instantiate(Function):
     def GetResponse(self, HubbeBot, message):
         if message.Type == 'JOIN':
             if message.User.Name.startswith("RoBoBo"):
-                UnloadFunction("Rustle")
-                return IRCResponse(ResponseType.Say, "Rustle unloaded.", message.ReplyTo)
+                UnloadFunction("IdentCheck")
+                return IRCResponse(ResponseType.Say, "IdentCheck unloaded.", message.ReplyTo)
         if message.Type == 'QUIT' or message.Type == 'PART':
             if message.User.Name.startswith("RoBoBo"):
-                LoadFunction("Rustle")
-                return IRCResponse(ResponseType.Say, "Rustle loaded.", message.ReplyTo)
+                LoadFunction("IdentCheck")
+                return IRCResponse(ResponseType.Say, "IdentCheck loaded.", message.ReplyTo)
--- Functions/Rustle.py
+++ Functions/Rustle.py
@@ -9,45 +9,7 @@ class Instantiate(Function):
     def GetResponse(self, HubbeBot, message):
         if message.Type != "PRIVMSG":
             return
-        if message.MessageString.lower().startswith("meow"):
-            roll = random.randint(1,20)
-            if (message.User.Name == "BillTheCat"):
-                return IRCResponse(ResponseType.Say, "Uhm... Hi?", message.ReplyTo)
-            if message.User.Name.startswith("Caitiri") or message.User.Name == "Caity":
-                return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "KITTEH!"', message.ReplyTo)
-            elif (roll == 1):
-                reroll = random.randint(1,20)
-                if (reroll == 20):
-                    return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "CRITICAL PUPPEH!"', message.ReplyTo), IRCResponse(ResponseType.Say, "Wait, what?", message.ReplyTo)
-                else:
-                    return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "NOT KITTEH."', message.ReplyTo)
-            elif (roll > 1) and (roll < 8):
-                return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "NOT KITTEH."', message.ReplyTo)
-            elif (roll > 7) and (roll < 14):
-                return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "MEHBEH KITTEH?"', message.ReplyTo)
-            elif (roll > 13) and (roll < 20):
-                return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "KITTEH!"', message.ReplyTo)
-            else:
-                return IRCResponse(ResponseType.Do, "points at " + message.User.Name + ', "CRITICAL KITTEH!"', message.ReplyTo)
-        if message.MessageString.lower().startswith("rawr"):
-            roll = random.randint(1,20)
-            if (message.User.Name == "Itazu") or (message.User.Name == "Trahsi") or (message.User.Name == "reptile"):
-                return IRCResponse(ResponseType.Say, message.User.Name + " is a DRAGON!" , message.ReplyTo)
-            elif (roll == 1):
-                reroll = random.randint(1,20)
-                if (reroll == 20):
-                    return IRCResponse(ResponseType.Say, message.User.Name + " is SECRETLY A DRAGON!", message.ReplyTo)
-                else:
-                    return IRCResponse(ResponseType.Say, message.User.Name + " is NOT a DINOSAUR.", message.ReplyTo)
-            elif (roll > 1) and (roll < 8):
-                return IRCResponse(ResponseType.Say, message.User.Name + " is NOT a DINOSAUR.", message.ReplyTo)
-            elif (roll > 7) and (roll < 14):
-                return IRCResponse(ResponseType.Say, message.User.Name + " MIGHT be a DINOSAUR.", message.ReplyTo)
-            elif (roll > 13) and (roll < 20):
-                return IRCResponse(ResponseType.Say, message.User.Name + " is a DINOSAUR.", message.ReplyTo)
-            else:
-                return IRCResponse(ResponseType.Say, message.User.Name + " is a CRITICAL DINOSAUR!", message.ReplyTo)
-
+        
         if message.Command == "rustle":
             if len(message.ParameterList) != 1:
                 return IRCResponse(ResponseType.Say, "Rustle who?", message.ReplyTo)
