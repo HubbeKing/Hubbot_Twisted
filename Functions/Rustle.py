ea7355cc712d5ac59c218a34ca4ac76e3ab91081
Hubbe King <hubbe128@gmail.com>
2014-03-24 12:02:47 +0200

    JimmIES not jimmes

--- Functions/Rustle.py
+++ Functions/Rustle.py
@@ -18,8 +18,8 @@ class Instantiate(Function):
                 if roll == 1:
                     return IRCResponse(ResponseType.Say, message.User.Name + " has rustled their own jimmies in their critical failure!", message.ReplyTo)
                 elif  (roll > 1) and (roll < 12):
-                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmes status: unrustled", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmies status: unrustled", message.ReplyTo)
                 elif (roll > 11) and (roll < 20):
-                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmes status: rustled", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmies status: rustled", message.ReplyTo)
                 else:
-                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmes status: CRITICAL RUSTLE", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmies status: CRITICAL RUSTLE", message.ReplyTo)
