6e907461f136c41c32c8fb1efa455c74e7768bad
Hubbe King <hubbe128@gmail.com>
2014-04-22 21:08:46 +0300

    Rustle more than one word at once.

--- Functions/Rustle.py
+++ Functions/Rustle.py
@@ -1,7 +1,7 @@
 from IRCMessage import IRCMessage
 from IRCResponse import IRCResponse, ResponseType
 from Function import Function
-import random
+import random, string
 
 class Instantiate(Function):
     Help = "rustle <rustlee> - There's no need to be upset."
@@ -11,15 +11,15 @@ class Instantiate(Function):
             return
         
         if message.Command == "rustle":
-            if len(message.ParameterList) != 1:
+            if len(message.ParameterList) < 1:
                 return IRCResponse(ResponseType.Say, "Rustle who?", message.ReplyTo)
             else:
                 roll = random.randint(1,20)
                 if roll == 1:
                     return IRCResponse(ResponseType.Say, message.User.Name + " has rustled their own jimmies in their critical failure!", message.ReplyTo)
                 elif  (roll > 1) and (roll < 12):
-                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmies status: unrustled", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, string.join(message.ParameterList) + "'s jimmies status: unrustled", message.ReplyTo)
                 elif (roll > 11) and (roll < 20):
-                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmies status: rustled", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, string.join(message.ParameterList) + "'s jimmies status: rustled", message.ReplyTo)
                 else:
-                    return IRCResponse(ResponseType.Say, message.ParameterList[0] + "'s jimmies status: CRITICAL RUSTLE", message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, string.join(message.ParameterList) + "'s jimmies status: CRITICAL RUSTLE", message.ReplyTo)
