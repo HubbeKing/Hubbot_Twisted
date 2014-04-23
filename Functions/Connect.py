0049720a4323cb4a7421d77c3362bd36954c2d4f
Hubbe King <hubbe128@gmail.com>
2014-04-23 10:59:30 +0300

    Default to port 6667 if no port specified

--- Functions/Connect.py
+++ Functions/Connect.py
@@ -12,8 +12,12 @@ class Instantiate(Function):
         if message.Command == "connect" and message.User.Name in GlobalVars.admins:
             if len(message.ParameterList)>=2:
                 server_with_port = message.ParameterList[0]
-                server = server_with_port.split(":")[0]
-                port = int(server_with_port.split(":")[1])
+                if ":" in server_with_port:
+                    server = server_with_port.split(":")[0]
+                    port = int(server_with_port.split(":")[1])
+                else:
+                    server = server_with_port
+                    port = 6667
                 tmpChannels = message.ParameterList[1:]
                 channels = []
                 for chan in tmpChannels:
