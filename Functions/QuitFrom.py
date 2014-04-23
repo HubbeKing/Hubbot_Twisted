c1c498bdf9f98a02f6c6a1f4a005ea377e3e89d5
Hubbe King <hubbe128@gmail.com>
2014-04-23 09:47:58 +0300

    Typo and an oopsie

--- Functions/QuitFrom.py
+++ Functions/QuitFrom.py
@@ -4,7 +4,7 @@ from Function import Function
 import GlobalVars
 
 class Instantiate(Function):
-    Help = "quitfrom <server> - disonnects from the given server"
+    Help = "quitfrom <server> - disconnects from the given server"
 
     def GetResponse(self, Hubbot, message):
         if message.Type != "PRIVMSG":
@@ -12,4 +12,5 @@ class Instantiate(Function):
         if message.Command == "quitfrom" and message.User.Name in GlobalVars.admins:
             if len(message.ParameterList)>=1:
                 for server in message.ParameterList:
+                    GlobalVars.bothandler.botfactories[server].protocol.Quitting = True
                     GlobalVars.bothandler.stopBotFactory(server)
