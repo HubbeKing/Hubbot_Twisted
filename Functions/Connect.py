6ddc8797035eba9f8c118e075e9b6d45081b3a9e
Hubbe King <hubbe128@gmail.com>
2014-04-23 10:25:16 +0300

    Allow multiple channels in connect message
    Still unicode-error, though? WHAT

--- Functions/Connect.py
+++ Functions/Connect.py
@@ -10,7 +10,7 @@ class Instantiate(Function):
         if message.Type != "PRIVMSG":
             return
         if message.Command == "connect" and message.User.Name in GlobalVars.admins:
-            if len(message.ParameterList)==2:
+            if len(message.ParameterList)>=2:
                 server_with_port = message.ParameterList[0]
                 server = server_with_port.split(":")[0]
                 port = int(server_with_port.split(":")[1])
