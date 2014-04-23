a6e4d06632f14c21d0c9874ca51e754be88ac7c8
Hubbe King <hubbe128@gmail.com>
2014-04-23 10:00:36 +0300

    Only allow one channel in connect
    Otherwise we get unicode-error? Why do we get unicode-error. I don't.
    What?

--- Functions/Connect.py
+++ Functions/Connect.py
@@ -4,13 +4,13 @@ from Function import Function
 import GlobalVars
 
 class Instantiate(Function):
-    Help = "connect <server:port> <channel> [channel] - connect to a new server"
+    Help = "connect <server:port> <channel> - connect to a new server"
 
     def GetResponse(self, Hubbot, message):
         if message.Type != "PRIVMSG":
             return
         if message.Command == "connect" and message.User.Name in GlobalVars.admins:
-            if len(message.ParameterList)>=2:
+            if len(message.ParameterList)==2:
                 server_with_port = message.ParameterList[0]
                 server = server_with_port.split(":")[0]
                 port = int(server_with_port.split(":")[1])
