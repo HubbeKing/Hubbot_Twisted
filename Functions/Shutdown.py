d793c272b604b047e20edade147d2ec56a50d0d1
Hubbe King <hubbe128@gmail.com>
2014-04-23 15:30:00 +0300

    Ignore shutdown commands in the first 10s

--- Functions/Shutdown.py
+++ Functions/Shutdown.py
@@ -2,6 +2,7 @@ from IRCResponse import IRCResponse, ResponseType
 from IRCMessage import IRCMessage
 from Function import Function
 import GlobalVars
+import datetime
 
 class Instantiate(Function):
     Help = "shutdown - shut down all the bots everywhere"
@@ -9,5 +10,5 @@ class Instantiate(Function):
     def GetResponse(self, Hubbot, message):
         if message.Type != "PRIVMSG":
             return
-        if message.Command == "shutdown" and message.User.Name in GlobalVars.admins:
+        if message.Command == "shutdown" and datetime.datetime.now() > Hubbot.startTime + datetime.timedelta(seconds=10) and message.User.Name in GlobalVars.admins:
             GlobalVars.bothandler.shutdown()
