c9accc9a7949980933471be306dc45df3dc35b4a
Hubbe King <hubbe128@gmail.com>
2014-05-11 16:55:42 +0300

    An oops in RoBoBo
    Also these .idea files can GTFO

--- Functions/RoBoBo.py
+++ Functions/RoBoBo.py
@@ -9,9 +9,9 @@ class Instantiate(Function):
     def GetResponse(self, HubbeBot, message):
         if message.Type == 'JOIN':
             if message.User.Name.startswith("RoBoBo"):
-                UnloadFunction("IdentCheck")
+                UnloadFunction("identcheck")
                 return IRCResponse(ResponseType.Say, "IdentCheck unloaded.", message.ReplyTo)
         if message.Type == 'QUIT' or message.Type == 'PART':
             if message.User.Name.startswith("RoBoBo"):
-                LoadFunction("IdentCheck")
+                LoadFunction("identcheck")
                 return IRCResponse(ResponseType.Say, "IdentCheck loaded.", message.ReplyTo)
