82766d8a3441027bb1c499106480b36b5b557134
Hubbe King <hubbe128@gmail.com>
2014-05-12 11:59:49 +0300

    Use dicts rather than walls of elif
    Neater!

--- Commands/Servers.py
+++ Commands/Servers.py
@@ -4,38 +4,35 @@ import GlobalVars
 
 
 class Command(CommandInterface):
-    triggers = ["servers", "mumble", "gmod", "starbound", "starbound2", "jcmp", "tetri", "cockatrice", "kf", "<server>"]
     Help = "mumble, gmod, starbound, starbound2, jcmp, tetri, cockatrice, kf -- Used to post server info for games! Usage: {}<server>".format(GlobalVars.CommandChar)
-    mumbleIP = 'The mumble server is hosted at: mumble.dahou.se'
     gmodMods = 'List of mods needed for GMOD: http://steamcommunity.com/sharedfiles/filedetails/?id=185811989'
     gmodIP = "The Garrys Mod server is hosted at: gmod.dahou.se"
-    starboundIP = "The Starbound server is hosted at: starbound.dahou.se"
-    jcmpIP = "Ricin's Just Cause 2 MP server is hosted at: jcmp.117.me"
-    ricinStarboundIP = "Ricin's Starbound server is hosted at: sb.117.me"
-    tetrinetIP = "Ricin's Tetrinet server is hosted at: tn.ricin.us"
-    cockatriceIP = "The Cockatrice server is hosted at: cockatrice.dahou.se:4747"
-    killingfloorIP = "The Killing Floor server is hosted at: kf.dahou.se"
 
-    filename = "data/data.db"
+    def onStart(self):
+        serverDict = \
+            {
+                "servers":"",
+                "mumble":'The mumble server is hosted at: mumble.dahou.se',
+                "gmod":"",
+                "starbound":"The Starbound server is hosted at: starbound.dahou.se",
+                "starbound2":"Ricin's Starbound server is hosted at: sb.117.me",
+                "jcmp":"Ricin's Just Cause 2 MP server is hosted at: jcmp.117.me",
+                "tetri":"Ricin's Tetrinet server is hosted at: tn.ricin.us",
+                "cockatrice":"The Cockatrice server is hosted at: cockatrice.dahou.se:4747",
+                "kf":"The Killing Floor server is hosted at: kf.dahou.se"
+            }
+        self.triggers.extend(serverDict.keys())
+
+    def shouldExecute(self, message):
+        if message.Command in self.serverDict and message.Type in self.acceptedTypes:
+            return True
+        else:
+            return False
 
     def execute(self, Hubbot, message):
         if message.Command == "servers":
             return IRCResponse(ResponseType.Say, self.Help, message.ReplyTo)
-        if message.Command == "mumble":
-            return IRCResponse(ResponseType.Say, self.mumbleIP, message.ReplyTo)
         elif message.Command == "gmod":
             return IRCResponse(ResponseType.Say, self.gmodMods, message.ReplyTo), IRCResponse(ResponseType.Say, self.gmodIP, message.ReplyTo)
-        elif message.Command == "starbound":
-            return IRCResponse(ResponseType.Say, self.starboundIP, message.ReplyTo)
-        elif message.Command == "starbound2":
-            return IRCResponse(ResponseType.Say, self.ricinStarboundIP, message.ReplyTo)
-        elif message.Command == "jcmp":
-            return IRCResponse(ResponseType.Say, self.jcmpIP, message.ReplyTo)
-        elif message.Command == "tetri":
-            return IRCResponse(ResponseType.Say, self.tetrinetIP, message.ReplyTo)
-        elif message.Command == "cockatrice":
-            return IRCResponse(ResponseType.Say, self.cockatriceIP, message.ReplyTo)
-        elif message.Command == "kf":
-            return IRCResponse(ResponseType.Say, self.killingfloorIP, message.ReplyTo)
-        elif message.Command == "<server>":
-            return IRCResponse(ResponseType.Say, "Seriously?", message.ReplyTo)
+        else:
+            return IRCResponse(ResponseType.Say, self.serverDict[message.Command], message.ReplyTo)
--- Commands/Silly.py
+++ Commands/Silly.py
@@ -4,44 +4,44 @@ import GlobalVars
 
 
 class Command(CommandInterface):
-    triggers = ["donotwant", "yes", "yup", "store", "goat", "stupid", "fixyou", "heya", "hngh", "ps", "psc", "whoa", "what", "mybrand", "both", "no", "<thing>", "disappoint"]
     Help = "donotwant, yes, yup, store, goat, stupid, fixyou, heya, HNGH, PS, whoa, what, mybrand, both, no, disappoint -- Used to post silly things! Usage: {}<thing>".format(GlobalVars.CommandChar)
+
+    def onStart(self):
+        linkDict = \
+            {
+                "silly":"",
+                "nope":"http://www.youtube.com/watch?v=gvdf5n-zI14",
+                "donotwant":"http://www.youtube.com/watch?v=oKI-tD0L18A",
+                "yes":"http://www.youtube.com/watch?v=P3ALwKeSEYs",
+                "store":"http://www.youtube.com/watch?v=iRZ2Sh5-XuM",
+                "stupid":"http://www.youtube.com/watch?v=yytbDZrw1jc",
+                "fixyou":"http://www.youtube.com/watch?v=oin0KNElSG0",
+                "yup":"http://www.youtube.com/watch?v=K0QHw7iy1Rg",
+                "heya":"http://www.youtube.com/watch?v=ZZ5LpwO-An4",
+                "what":"http://www.youtube.com/watch?v=PDXrXBsTFSE",
+                "goat":"http://www.youtube.com/watch?v=SIaFtAKnqBU",
+                "mybrand":"http://www.youtube.com/watch?v=V-fRuoMIfpw",
+                "whoa":"http://www.youtube.com/watch?v=6q-gHrEaEAs",
+                "hngh":"http://24.media.tumblr.com/tumblr_malzltlKk21rggqwfo1_500.gif",
+                "ps":"https://docs.google.com/document/d/1C4Maba3fXrLYMMIXK67OOn7C2c31V6nff5inGAZStsw/edit?pli=1",
+                "psc":"https://docs.google.com/document/d/1C4Maba3fXrLYMMIXK67OOn7C2c31V6nff5inGAZStsw/edit?pli=1",
+                "both":"http://www.youtube.com/watch?v=OawrlVoQqSs",
+                "no":"http://www.youtube.com/watch?v=YKss2uYpih8",
+                "hunt":"",
+                "<thing>":"Har Har.",
+                "dissapoint":"https://31.media.tumblr.com/cea6574a24b490ada8bec694e87b307b/tumblr_n3blu9k5CW1tsipf6o6_400.gif"
+            }
+        self.triggers.extend(linkDict.keys())
+
+    def shouldExecute(self, message):
+        if message.Command in self.linkDict.keys and message.Type in self.acceptedTypes:
+            return True
+        else:
+            return False
 
     def execute(self, Hubbot, message):
         if message.Command == "silly":
             return IRCResponse(ResponseType.Say, self.Help, message.ReplyTo)
-        if message.Command == "nope":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=gvdf5n-zI14", message.ReplyTo)
-        elif message.Command == "donotwant":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=oKI-tD0L18A", message.ReplyTo)
-        elif message.Command == "yes":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=P3ALwKeSEYs", message.ReplyTo)
-        elif message.Command == "store":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=iRZ2Sh5-XuM", message.ReplyTo)
-        elif message.Command == "stupid":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=yytbDZrw1jc", message.ReplyTo)
-        elif message.Command == "fixyou":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=oin0KNElSG0", message.ReplyTo)
-        elif message.Command == "yup":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=K0QHw7iy1Rg", message.ReplyTo)
-        elif message.Command == "heya":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=ZZ5LpwO-An4", message.ReplyTo)
-        elif message.Command == "what":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=PDXrXBsTFSE", message.ReplyTo)
-        elif message.Command == "goat":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=SIaFtAKnqBU", message.ReplyTo)
-        elif message.Command == "mybrand":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=V-fRuoMIfpw", message.ReplyTo)
-        elif message.Command == "whoa":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=6q-gHrEaEAs", message.ReplyTo)
-        elif message.Command == "hngh":
-            return IRCResponse(ResponseType.Say, "http://24.media.tumblr.com/tumblr_malzltlKk21rggqwfo1_500.gif", message.ReplyTo)
-        elif message.Command == "ps" or message.Command == "psc":
-            return IRCResponse(ResponseType.Say, "https://docs.google.com/document/d/1C4Maba3fXrLYMMIXK67OOn7C2c31V6nff5inGAZStsw/edit?pli=1", message.ReplyTo)
-        elif message.Command == "both":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=OawrlVoQqSs", message.ReplyTo)
-        elif message.Command == "no":
-            return IRCResponse(ResponseType.Say, "http://www.youtube.com/watch?v=YKss2uYpih8", message.ReplyTo)
         elif message.Command == "hunt":
             if message.User.Name in GlobalVars.admins:
                 line1 = "LAPTOP CHAT ENGAGE"
@@ -49,7 +49,5 @@ class Command(CommandInterface):
                 line3 = "FLOOD TUBES THREE AND FOUR"
                 line4 = "HUNT FOR RED OCTOBER"
                 return IRCResponse(ResponseType.Say, line1, message.ReplyTo), IRCResponse(ResponseType.Say, line2, message.ReplyTo), IRCResponse(ResponseType.Say, line3, message.ReplyTo), IRCResponse(ResponseType.Say, line4, message.ReplyTo)
-        elif message.Command == "<thing>":
-            return IRCResponse(ResponseType.Say, "Har har.", message.ReplyTo)
-        elif message.Command.startswith("disappoint"):
-            return IRCResponse(ResponseType.Say, "https://31.media.tumblr.com/cea6574a24b490ada8bec694e87b307b/tumblr_n3blu9k5CW1tsipf6o6_400.gif", message.ReplyTo)
+        else:
+            return IRCResponse(ResponseType.Say, self.linkDict[message.Command], message.ReplyTo)
