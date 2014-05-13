58ebcae3657853df5b4c712f3592a19522d9928a
Hubbe King <hubbe128@gmail.com>
2014-05-13 10:10:04 +0300

    Help should be more helpful.

--- Commands/Help.py
+++ Commands/Help.py
@@ -5,7 +5,7 @@ import GlobalVars
 
 class Command(CommandInterface):
     triggers = ['help','command','commands']
-    help = 'command(s)/help/function(s) (<function>) - returns a list of loaded commands, or the help text of a particular command if one is specified'
+    help = 'command(s)/help/function(s) (<function>) - returns a list of loaded modules, or the help text of a particular command if one is specified'
     
     def execute(self, Hubbot, message):
         if len(message.ParameterList) > 0:
@@ -16,8 +16,9 @@ class Command(CommandInterface):
                 else:
                     return IRCResponse(ResponseType.Say, func.help(message), message.ReplyTo)
             else:
-                return IRCResponse(ResponseType.Say, '"{}" not found, try "{}" without parameters to see a list of loaded commands'.format(message.ParameterList[0],message.Command), message.ReplyTo)
+                return IRCResponse(ResponseType.Say, '"{}" not found, try "{}" without parameters to see a list of loaded modules'.format(message.ParameterList[0],message.Command), message.ReplyTo)
         else:
             funcs = ', '.join(sorted(GlobalVars.commands.iterkeys(), key=lambda s: s.lower()))
-            return [IRCResponse(ResponseType.Say, "Commands loaded are:", message.ReplyTo),
-                    IRCResponse(ResponseType.Say, funcs, message.ReplyTo)]
+            return [IRCResponse(ResponseType.Say, "Modules loaded are:", message.ReplyTo),
+                    IRCResponse(ResponseType.Say, funcs, message.ReplyTo),
+                    IRCResponse(ResponseType.Say, "Use {}help <module> for module commands.".format(GlobalVars.CommandChar), message.ReplyTo)]
