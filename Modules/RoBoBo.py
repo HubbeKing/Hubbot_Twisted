613f7bd0f8fc37a87db2d22c22aeeedccad689b0
Hubbe King <hubbe128@gmail.com>
2014-05-15 11:18:54 +0300

    Fuckin hell why isn't this working

--- Modules/RoBoBo.py
+++ Modules/RoBoBo.py
@@ -1,6 +1,6 @@
 from IRCResponse import IRCResponse, ResponseType
 from ModuleInterface import ModuleInterface
-import ModuleLoader
+from ModuleHandler import LoadModule, UnloadModule
 
 
 class Module(ModuleInterface):
@@ -10,9 +10,9 @@ class Module(ModuleInterface):
     def onTrigger(self, Hubbot, message):
         if message.Type == 'JOIN':
             if message.User.Name.startswith("RoBoBo"):
-                ModuleLoader.unload("IdentCheck")
+                UnloadModule("IdentCheck")
                 return IRCResponse(ResponseType.Say, "IdentCheck unloaded.", message.ReplyTo)
         if message.Type == 'QUIT' or message.Type == 'PART':
             if message.User.Name.startswith("RoBoBo"):
-                ModuleLoader.load("IdentCheck")
+                LoadModule("IdentCheck")
                 return IRCResponse(ResponseType.Say, "IdentCheck loaded.", message.ReplyTo)
