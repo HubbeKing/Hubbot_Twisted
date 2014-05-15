c868805f89d092d9da1d710af0c0408ca0a4fa10
Hubbe King <hubbe128@gmail.com>
2014-05-15 19:22:53 +0300

    Tweak triggering for Grapheme module.

--- Modules/Grapheme.py
+++ Modules/Grapheme.py
@@ -7,10 +7,11 @@ class Module(ModuleInterface):
     help = "grapheme -- for when you need someone to talk to."
 
     def shouldTrigger(self, message):
-        if message.messageString.startsWith(GlobalVars.CurrentNick):
+        if self.MessageList[0].startswith(GlobalVars.CurrentNick) and len(self.MessageList) > 1:
             return True
         else:
             return False
 
     def onTrigger(self, Hubbot, message):
-        return IRCResponse(ResponseType.Say, Hubbot.brain.get_reply(message.messageString), message.replyTo)
+        messageToUse = message.messageList[1:]
+        return IRCResponse(ResponseType.Say, Hubbot.brain.get_reply(messageToUse), message.replyTo)
