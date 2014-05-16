998df32872b6523177e34d5397448c69350b200f
Hubbe King <hubbe128@gmail.com>
2014-05-16 21:50:32 +0300

    [Grapheme] Try to get it to UnicodeWarning less

--- Modules/Grapheme.py
+++ Modules/Grapheme.py
@@ -7,13 +7,13 @@ class Module(ModuleInterface):
     help = "grapheme -- for when you need someone to talk to."
 
     def shouldTrigger(self, message):
-        if message.Type = "PRIVMSG" and message.MessageList[0].startswith(GlobalVars.CurrentNick) and len(message.MessageList) > 1:
+        if message.Type == "PRIVMSG" and message.MessageList[0].startswith(GlobalVars.CurrentNick) and len(message.MessageList) > 1:
             return True
         else:
             return False
 
     def onTrigger(self, Hubbot, message):
         stringToUse = " ".join(message.MessageList[1:])
-        messageToUse = u'{}'.format(stringToUse)
+        messageToUse = stringToUse.encode("utf-8")
         reply = u'{}'.format(Hubbot.brain.get_reply(messageToUse[:180].rsplit(".")[0]))
         return IRCResponse(ResponseType.Say, reply, message.ReplyTo)
