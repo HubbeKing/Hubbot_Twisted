109570052a0b70c553324bb2f287cec5a0b9c0c9
Hubbe King <hubbe128@gmail.com>
2014-05-16 21:45:53 +0300

    [Grapheme] Only PRIVMSG should trigger.

--- Modules/Grapheme.py
+++ Modules/Grapheme.py
@@ -7,7 +7,7 @@ class Module(ModuleInterface):
     help = "grapheme -- for when you need someone to talk to."
 
     def shouldTrigger(self, message):
-        if message.MessageList[0].startswith(GlobalVars.CurrentNick) and len(message.MessageList) > 1:
+        if message.Type = "PRIVMSG" and message.MessageList[0].startswith(GlobalVars.CurrentNick) and len(message.MessageList) > 1:
             return True
         else:
             return False
