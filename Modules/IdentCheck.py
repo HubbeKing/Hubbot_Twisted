478128a6d56095bc2dca0863332229768737775d
Hubbe King <hubbe128@gmail.com>
2014-05-16 19:44:55 +0300

    Attempt to make IdentCheck use the userlist

--- Modules/IdentCheck.py
+++ Modules/IdentCheck.py
@@ -10,41 +10,42 @@ class Module(ModuleInterface):
         return True
 
     def onTrigger(self, Hubbot, message):
-        if message.MessageString.lower().startswith("meow"):
-            roll = random.randint(1,20)
-            if message.User.Name == "BillTheCat":
-                return IRCResponse(ResponseType.Say, "Uhm... Hi?", message.ReplyTo)
-            if message.User.Name.startswith("Caitiri") or message.User.Name == "Caity":
-                return IRCResponse(ResponseType.Do, 'points at {}, "KITTEH!"'.format(message.User.Name), message.ReplyTo)
-            elif roll == 1:
-                reroll = random.randint(1,20)
-                if reroll == 20:
-                    return IRCResponse(ResponseType.Do, 'points at {}, "CRITICAL PUPPEH!"'.format(message.User.Name), message.ReplyTo), IRCResponse(ResponseType.Say, "Wait, what?", message.ReplyTo)
-                else:
+        if "RoBoBo" not in Hubbot.channels[message.ReplyTo].Users.keys():
+            if message.MessageString.lower().startswith("meow"):
+                roll = random.randint(1,20)
+                if message.User.Name == "BillTheCat":
+                    return IRCResponse(ResponseType.Say, "Uhm... Hi?", message.ReplyTo)
+                if message.User.Name.startswith("Caitiri") or message.User.Name == "Caity":
+                    return IRCResponse(ResponseType.Do, 'points at {}, "KITTEH!"'.format(message.User.Name), message.ReplyTo)
+                elif roll == 1:
+                    reroll = random.randint(1,20)
+                    if reroll == 20:
+                        return IRCResponse(ResponseType.Do, 'points at {}, "CRITICAL PUPPEH!"'.format(message.User.Name), message.ReplyTo), IRCResponse(ResponseType.Say, "Wait, what?", message.ReplyTo)
+                    else:
+                        return IRCResponse(ResponseType.Do, 'points at {}, "NOT KITTEH."'.format(message.User.Name), message.ReplyTo)
+                elif (roll > 1) and (roll < 8):
                     return IRCResponse(ResponseType.Do, 'points at {}, "NOT KITTEH."'.format(message.User.Name), message.ReplyTo)
-            elif (roll > 1) and (roll < 8):
-                return IRCResponse(ResponseType.Do, 'points at {}, "NOT KITTEH."'.format(message.User.Name), message.ReplyTo)
-            elif (roll > 7) and (roll < 14):
-                return IRCResponse(ResponseType.Do, 'points at {}, "MEHBEH KITTEH?"'.format(message.User.Name), message.ReplyTo)
-            elif (roll > 13) and (roll < 20):
-                return IRCResponse(ResponseType.Do, 'points at {}, "KITTEH!"'.format(message.User.Name), message.ReplyTo)
-            else:
-                return IRCResponse(ResponseType.Do, 'points at {}, "CRITICAL KITTEH!"'.format(message.User.Name), message.ReplyTo)
-        if message.MessageString.lower().startswith("rawr"):
-            roll = random.randint(1,20)
-            if (message.User.Name == "Itazu") or (message.User.Name == "Trahsi") or (message.User.Name == "reptile"):
-                return IRCResponse(ResponseType.Say, "{} is a DRAGON!".format(message.User.Name) , message.ReplyTo)
-            elif roll == 1:
-                reroll = random.randint(1,20)
-                if reroll == 20:
-                    return IRCResponse(ResponseType.Say, " is SECRETLY A DRAGON!".format(message.User.Name), message.ReplyTo)
+                elif (roll > 7) and (roll < 14):
+                    return IRCResponse(ResponseType.Do, 'points at {}, "MEHBEH KITTEH?"'.format(message.User.Name), message.ReplyTo)
+                elif (roll > 13) and (roll < 20):
+                    return IRCResponse(ResponseType.Do, 'points at {}, "KITTEH!"'.format(message.User.Name), message.ReplyTo)
                 else:
+                    return IRCResponse(ResponseType.Do, 'points at {}, "CRITICAL KITTEH!"'.format(message.User.Name), message.ReplyTo)
+            if message.MessageString.lower().startswith("rawr"):
+                roll = random.randint(1,20)
+                if (message.User.Name == "Itazu") or (message.User.Name == "Trahsi") or (message.User.Name == "reptile"):
+                    return IRCResponse(ResponseType.Say, "{} is a DRAGON!".format(message.User.Name) , message.ReplyTo)
+                elif roll == 1:
+                    reroll = random.randint(1,20)
+                    if reroll == 20:
+                        return IRCResponse(ResponseType.Say, " is SECRETLY A DRAGON!".format(message.User.Name), message.ReplyTo)
+                    else:
+                        return IRCResponse(ResponseType.Say, "{} is NOT a DINOSAUR.".format(message.User.Name), message.ReplyTo)
+                elif (roll > 1) and (roll < 8):
                     return IRCResponse(ResponseType.Say, "{} is NOT a DINOSAUR.".format(message.User.Name), message.ReplyTo)
-            elif (roll > 1) and (roll < 8):
-                return IRCResponse(ResponseType.Say, "{} is NOT a DINOSAUR.".format(message.User.Name), message.ReplyTo)
-            elif (roll > 7) and (roll < 14):
-                return IRCResponse(ResponseType.Say, "{} MIGHT be a DINOSAUR.".format(message.User.Name), message.ReplyTo)
-            elif (roll > 13) and (roll < 20):
-                return IRCResponse(ResponseType.Say, "{} is a DINOSAUR.".format(message.User.Name), message.ReplyTo)
-            else:
-                return IRCResponse(ResponseType.Say, "{} is a CRITICAL DINOSAUR!".format(message.User.Name), message.ReplyTo)
+                elif (roll > 7) and (roll < 14):
+                    return IRCResponse(ResponseType.Say, "{} MIGHT be a DINOSAUR.".format(message.User.Name), message.ReplyTo)
+                elif (roll > 13) and (roll < 20):
+                    return IRCResponse(ResponseType.Say, "{} is a DINOSAUR.".format(message.User.Name), message.ReplyTo)
+                else:
+                    return IRCResponse(ResponseType.Say, "{} is a CRITICAL DINOSAUR!".format(message.User.Name), message.ReplyTo)
