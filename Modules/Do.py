dce4e41c6432765e0fb8c5e7fe64cde2b8edd5d3
Hubbe King <hubbe128@gmail.com>
2014-05-16 22:20:55 +0300

    [Do] Do module because Do module.

---
+++ Modules/Do.py
@@ -0,0 +1,13 @@
+from ModuleInterface import ModuleInterface
+from IRCResponse import IRCResponse, ResponseType
+
+
+class Module(ModuleInterface):
+    help = "do <thing> -- do a thing."
+    triggers = ["do"]
+
+    def onTrigger(self, Hubbot, message):
+        if len(message.ParameterList) == 0:
+            return IRCResponse(ResponseType.Say, "Do what?", message.ReplyTo)
+        else:
+            return IRCResponse(ResponseType.Do, "{}".format(" ".join(message.ParameterList)), message.ReplyTo)
