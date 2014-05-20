aebeb4cf06fcd7536f3e12f149e92c4a12f76cce
Hubbe King <hubbe128@gmail.com>
2014-05-20 15:35:07 +0300

    [Core] Join message handler with module handler

--- ModuleHandler.py
+++ ModuleHandler.py
@@ -1,8 +1,58 @@
 import importlib
-import os, sys
+import os, sys, traceback
 from glob import glob
+from IRCResponse import ResponseType
 import GlobalVars
 
+
+class MessageHandler(object):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def sendResponse(self, response):
+        responses = []
+
+        if hasattr(response, "__iter__"):
+            for r in response:
+                if r is None or r.Response is None or r.Response == "":
+                    continue
+                responses.append(r)
+        elif response is not None and response.Response is not None and response.Response != "":
+            responses.append(response)
+
+        for response in responses:
+            try:
+                if response.Type == ResponseType.Say:
+                    self.bot.msg(response.Target, response.Response.encode("utf-8"))
+                elif response.Type == ResponseType.Do:
+                    self.bot.describe(response.Target, response.Response.encode("utf-8"))
+                elif response.Type == ResponseType.Notice:
+                    self.bot.notice(response.Target, response.Response.encode("utf-8"))
+                elif response.Type == ResponseType.Raw:
+                    self.bot.sendLine(response.Response.encode("utf-8"))
+            except Exception:
+                print "Python Execution Error sending responses '{}': {}".format(responses, str(sys.exc_info()))
+                traceback.print_tb(sys.exc_info()[2])
+
+
+    def handleMessage(self, message):
+        for name, module in GlobalVars.modules.iteritems():
+            try:
+                if module.hasAlias(message):
+                    message = message.aliasedMessage()
+                if module.shouldTrigger(message):
+                    response = module.onTrigger(self.bot, message)
+                    if response is None:
+                        continue
+                    if hasattr(response, "__iter__"):
+                        for r in response:
+                            self.sendResponse(r)
+                    else:
+                        self.sendResponse(response)
+            except Exception:
+                print "Python Execution Error in '{}': {}".format(module.__name__, str(sys.exc_info()))
+                traceback.print_tb(sys.exc_info()[2])
 
 def LoadModule(name, loadAs=''):
 
@@ -40,7 +90,6 @@ def LoadModule(name, loadAs=''):
 
     return True
 
-
 def UnloadModule(name):
 
     if name.lower() in GlobalVars.moduleCaseMapping.keys():
@@ -55,7 +104,6 @@ def UnloadModule(name):
 
     return True
 
-
 def AutoLoadModules():
 
     for module in GetModuleDirList():
@@ -64,7 +112,6 @@ def AutoLoadModules():
                 LoadModule(module)
             except Exception, x:
                 print x.args
-
 
 def GetModuleDirList():
 
