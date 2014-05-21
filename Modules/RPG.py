63eca4f4d59f4568929cf5da8ee3dc013712f9d9
Hubbe King <hubbe128@gmail.com>
2014-05-21 15:10:56 +0300

    [RPG] Less code reuse, more functions!

--- Modules/RPG.py
+++ Modules/RPG.py
@@ -8,171 +8,96 @@ from WebUtils import pasteEE
 class Module(ModuleInterface):
     triggers = ["pf", "lp", "mm", "welch"]
     help = 'pf/lp/mm/welch <number>/add <thing>/list -- "helpful" RPG advice and stuff'
+    filename = "data/data.db"
 
     def onTrigger(self, Hubbot, message):
-        filename = "data/data.db"
         if message.Command == "pf":
             if len(message.ParameterList) == 0:
-                messageDict = {}
-                with sqlite3.connect(filename) as conn:
-                    c = conn.cursor()
-                    for row in c.execute("SELECT * FROM pathfinder"):
-                        messageDict[row[0]] = row[1]
-                choice = random.choice(messageDict.keys())
-                string = "{}. {}".format(str(choice), messageDict[choice])
-                return IRCResponse(ResponseType.Say, string, message.ReplyTo)
+                return IRCResponse(ResponseType.Say, self.getRandom("pathfinder"), message.ReplyTo)
             else:
                 if message.ParameterList[0] == "list":
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM pathfinder"):
-                            messageDict[row[0]] = row[1]
-                    pasteString = ""
-                    for number, string in messageDict.iteritems():
-                        pasteString += str(number) + ". " + string +"\n"
-                    pasteLink = pasteEE(pasteString, "Pathfinder", 10)
-                    return IRCResponse(ResponseType.Say, "Link posted! {}".format(pasteLink), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getList("pathfinder", "Pathfinder"), message.ReplyTo)
                 elif message.ParameterList[0] == "add":
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        c.execute("SELECT max(id) FROM pathfinder")
-                        maxNumber = c.fetchone()[0]
-                        c.execute("INSERT INTO pathfinder VALUES (?,?)", (maxNumber+1, " ".join(message.ParameterList[1:])))
-                        conn.commit()
+                    lineToAdd = " ".join(message.ParameterList[1:])
+                    newIndex = self.addLine("pathfinder", lineToAdd)
+                    return IRCResponse(ResponseType.Say, "Successfully added line '{}. {}'".format(newIndex, lineToAdd), message.ReplyTo)
                 else:
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM pathfinder"):
-                            messageDict[row[0]] = row[1]
-                    try:
-                        choice = int(message.ParameterList[0])
-                    except:
-                        return IRCResponse(ResponseType.Say, "I don't know what you mean by '{}'.".format(message.ParameterList[0]), message.ReplyTo)
-                    if choice in messageDict.keys():
-                        return IRCResponse(ResponseType.Say, "{}. {}".format(str(choice), messageDict[choice]), message.ReplyTo)
-                    else:
-                        return IRCResponse(ResponseType.Say, "Invalid number, valid numbers are <{}-{}>".format(messageDict.keys()[0], messageDict.keys()[-1]), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getSpecific("pathfinder", message.ParameterList[0]), message.ReplyTo)
         elif message.Command == "lp":
             if len(message.ParameterList) == 0:
-                messageDict = {}
-                with sqlite3.connect(filename) as conn:
-                    c = conn.cursor()
-                    for row in c.execute("SELECT * FROM lp"):
-                        messageDict[row[0]] = row[1]
-                choice = random.choice(messageDict.keys())
-                string = "{}. {}".format(str(choice), messageDict[choice])
-                return IRCResponse(ResponseType.Say, string, message.ReplyTo)
+               return IRCResponse(ResponseType.Say, self.getRandom("lp"), message.ReplyTo)
             else:
                 if message.ParameterList[0] == "list":
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM lp"):
-                            messageDict[row[0]] = row[1]
-                    pasteString = ""
-                    for number, string in messageDict.iteritems():
-                        pasteString += str(number) + ". " + string +"\n"
-                    pasteLink = pasteEE(pasteString, "Let's Play", 10)
-                    return IRCResponse(ResponseType.Say, "Link posted! {}".format(pasteLink), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getList("lp", "Let's Play"), message.ReplyTo)
                 elif message.ParameterList[0] == "add":
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        c.execute("SELECT max(id) FROM lp")
-                        maxNumber = c.fetchone()[0]
-                        c.execute("INSERT INTO lp VALUES (?,?)", (maxNumber+1, " ".join(message.ParameterList[1:])))
-                        conn.commit()
+                    lineToAdd = " ".join(message.ParameterList[1:])
+                    newIndex = self.addLine("lp", lineToAdd)
+                    return IRCResponse(ResponseType.Say, "Successfully added line '{}. {}'".format(newIndex, lineToAdd), message.ReplyTo)
                 else:
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM lp"):
-                            messageDict[row[0]] = row[1]
-                    try:
-                        choice = int(message.ParameterList[0])
-                    except:
-                        return IRCResponse(ResponseType.Say, "I don't know what you mean by '{}'.".format(message.ParameterList[0]), message.ReplyTo)
-                    if choice in messageDict.keys():
-                        return IRCResponse(ResponseType.Say, "{}. {}".format(str(choice), messageDict[choice]), message.ReplyTo)
-                    else:
-                        return IRCResponse(ResponseType.Say, "Invalid number, valid numbers are <{}-{}>".format(messageDict.keys()[0], messageDict.keys()[-1]), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getSpecific("lp", message.ParameterList[0]), message.ReplyTo)
         elif message.Command == "mm":
             if len(message.ParameterList) == 0:
-                messageDict = {}
-                with sqlite3.connect(filename) as conn:
-                    c = conn.cursor()
-                    for row in c.execute("SELECT * FROM mm"):
-                        messageDict[row[0]] = row[1]
-                choice = random.choice(messageDict.keys())
-                string = "{}. {}".format(str(choice), messageDict[choice])
-                return IRCResponse(ResponseType.Say, string, message.ReplyTo)
+                return IRCResponse(ResponseType.Say, self.getRandom("mm"), message.ReplyTo)
             else:
                 if message.ParameterList[0] == "list":
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM mm"):
-                            messageDict[row[0]] = row[1]
-                    pasteString = ""
-                    for number, string in messageDict.iteritems():
-                        pasteString += str(number) + ". " + string +"\n"
-                    pasteLink = pasteEE(pasteString, "Mutants & Masterminds", 10)
-                    return IRCResponse(ResponseType.Say, "Link posted! {}".format(pasteLink), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getList("mm", "Mutants & Masterminds"), message.ReplyTo)
                 elif message.ParameterList[0] == "add":
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        c.execute("SELECT max(id) FROM mm")
-                        maxNumber = c.fetchone()[0]
-                        c.execute("INSERT INTO mm VALUES (?,?)", (maxNumber+1, " ".join(message.ParameterList[1:])))
-                        conn.commit()
+                    lineToAdd = " ".join(message.ParameterList[1:])
+                    newIndex = self.addLine("mm", lineToAdd)
+                    return IRCResponse(ResponseType.Say, "Successfully added line '{}. {}'".format(newIndex, lineToAdd), message.ReplyTo)
                 else:
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM mm"):
-                            messageDict[row[0]] = row[1]
-                    try:
-                        choice = int(message.ParameterList[0])
-                    except:
-                        return IRCResponse(ResponseType.Say, "I don't know what you mean by '{}'.".format(message.ParameterList[0]), message.ReplyTo)
-                    if choice in messageDict.keys():
-                        return IRCResponse(ResponseType.Say, "{}. {}".format(str(choice), messageDict[choice]), message.ReplyTo)
-                    else:
-                        return IRCResponse(ResponseType.Say, "Invalid number, valid numbers are <{}-{}>".format(messageDict.keys()[0], messageDict.keys()[-1]), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getSpecific("mm", message.ParameterList[0]), message.ReplyTo)
         elif message.Command == "welch":
             if len(message.ParameterList) == 0:
-                messageDict = {}
-                with sqlite3.connect(filename) as conn:
-                    c = conn.cursor()
-                    for row in c.execute("SELECT * FROM welch"):
-                        messageDict[row[0]] = row[1]
-                choice = random.choice(messageDict.keys())
-                string = "{}. {}".format(str(choice), messageDict[choice])
-                return IRCResponse(ResponseType.Say, string, message.ReplyTo)
+                return IRCResponse(ResponseType.Say, self.getRandom("welch"), message.ReplyTo)
             else:
                 if message.ParameterList[0] == "list":
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM welch"):
-                            messageDict[row[0]] = row[1]
-                    pasteString = ""
-                    for number, string in messageDict.iteritems():
-                        pasteString += str(number) + ". " + string +"\n"
-                    pasteLink = pasteEE(pasteString, "Welch", 10)
-                    return IRCResponse(ResponseType.Say, "Link posted! {}".format(pasteLink), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getList("welch", "Welch"), message.ReplyTo)
                 else:
-                    messageDict = {}
-                    with sqlite3.connect(filename) as conn:
-                        c = conn.cursor()
-                        for row in c.execute("SELECT * FROM welch"):
-                            messageDict[row[0]] = row[1]
-                    try:
-                        choice = int(message.ParameterList[0])
-                    except:
-                        return IRCResponse(ResponseType.Say, "I don't know what you mean by '{}'.".format(message.ParameterList[0]), message.ReplyTo)
-                    if choice in messageDict.keys():
-                        return IRCResponse(ResponseType.Say, "{}. {}".format(str(choice), messageDict[choice]), message.ReplyTo)
-                    else:
-                        return IRCResponse(ResponseType.Say, "Invalid number, valid numbers are <{}-{}>".format(messageDict.keys()[0], messageDict.keys()[-1]), message.ReplyTo)
+                    return IRCResponse(ResponseType.Say, self.getSpecific("welch", message.ParameterList[0]), message.ReplyTo)
+
+    def getRandom(self, table):
+        messageDict = {}
+        with sqlite3.connect(self.filename) as conn:
+            c = conn.cursor()
+            for row in c.execute("SELECT * FROM {}".format(table)):
+                messageDict[row[0]] = row[1]
+        choice = random.choice(messageDict.keys())
+        string = "{}. {}".format(str(choice), messageDict[choice])
+        return string
+
+    def getSpecific(self, table, number):
+        messageDict = {}
+        with sqlite3.connect(self.filename) as conn:
+            c = conn.cursor()
+            for row in c.execute("SELECT * FROM {}".format(table)):
+                messageDict[row[0]] = row[1]
+        try:
+            choice = int(number)
+        except:
+            return "I don't know what you mean by '{}'.".format(number)
+        if choice in messageDict.keys():
+            return "{}. {}".format(str(choice), messageDict[choice])
+        else:
+            return "Invalid number, valid numbers are <{}-{}>".format(messageDict.keys()[0], messageDict.keys()[-1])
+
+    def getList(self, table, name):
+        messageDict = {}
+        with sqlite3.connect(self.filename) as conn:
+            c = conn.cursor()
+            for row in c.execute("SELECT * FROM {}".format(table)):
+                messageDict[row[0]] = row[1]
+        pasteString = ""
+        for number, string in messageDict.iteritems():
+            pasteString += str(number) + ". " + string + "\n"
+        pasteLink = pasteEE(pasteString, name, 10)
+        return "Link posted! {}".format(pasteLink)
+
+    def addLine(self, table, line):
+        with sqlite3.connect(self.filename) as conn:
+            c = conn.cursor()
+            c.execute("SELECT max(id) FROM {}".format(table))
+            maxNumber = c.fetchone()[0]
+            c.execute("INSERT INTO {} VALUES (?,?)".format(table), (maxNumber+1, line))
+            conn.commit()
+        return maxNumber + 1
