21dec0b99c60ac50c0324ce49f09ef6c8fa6e5fb
Hubbe King <hubbe128@gmail.com>
2014-05-22 18:30:38 +0300

    [Alias] Bugfixessss

--- Modules/Alias.py
+++ Modules/Alias.py
@@ -1,4 +1,4 @@
-import sqlite3, re
+import sqlite3
 from ModuleInterface import ModuleInterface
 from IRCResponse import IRCResponse, ResponseType
 from IRCMessage import IRCMessage
@@ -6,9 +6,10 @@ import GlobalVars
 
 
 class Alias(ModuleInterface):
+    triggers = ["alias"]
     help = 'alias <alias> <command> <params> - aliases <alias> to the specified command and parameters\n' \
            'you can specify where parameters given to the alias should be inserted with $1, $2, $n. ' \
-           'you can use $1+, $2+ for all parameters after the first, second one' \
+           'you can use $1+, $2+ for all parameters after the first, second one, etc. ' \
            'The whole parameter string is $0. $sender and $channel can also be used.'
     aliases = {}
 
@@ -31,16 +32,15 @@ class Alias(ModuleInterface):
     def aliasedMessage(self, message):
         if message.Command in self.aliases.keys():
             alias = self.aliases[message.Command]
-            newMsg = re.sub(message.Command, " ".join(alias), message.MessageString, count=1)
+            newMsg = message.MessageString.replace(message.Command, " ".join(alias), 1)
             if "$sender" in newMsg:
                 newMsg = newMsg.replace("$sender", message.User.Name)
             if "$channel" in newMsg:
                 newMsg = newMsg.replace("$channel", message.ChannelObj.Name)
+            newMsg = newMsg.replace(message.Parameters, "")
             if "$0" in newMsg:
-                newMsg = newMsg.replace(message.Parameters, "")
-                newMsg = newMsg.replace("$0", message.Parameters)
+                newMsg = newMsg.replace("$0", " ".join(message.ParameterList))
             if len(message.ParameterList) >= 1:
-                newMsg = newMsg.replace(message.Parameters, "")
                 for i, param in enumerate(message.ParameterList):
                     if newMsg.find("${}+".format(i+1)) != -1:
                         newMsg = newMsg.replace("${}+".format(i+1), " ".join(message.ParameterList[i:]))
@@ -49,7 +49,7 @@ class Alias(ModuleInterface):
             return IRCMessage(message.Type, message.User.String, message.ChannelObj, newMsg)
 
     def onTrigger(self, message):
-        if message.Command not in self.aliases.keys():
+        if message.Command in self.triggers:
             if message.User.Name not in GlobalVars.admins:
                 return IRCResponse(ResponseType.Say, "Only my admins may create new aliases!", message.ReplyTo)
 
@@ -74,5 +74,5 @@ class Alias(ModuleInterface):
             self.newAlias(message.ParameterList[0], newAlias)
 
             return IRCResponse(ResponseType.Say, "Created a new alias '{}' for '{}'.".format(message.ParameterList[0], " ".join(message.ParameterList[1:])), message.ReplyTo)
-        else:
+        elif message.Command in self.aliases.keys():
             self.bot.moduleHandler.handleMessage(self.aliasedMessage(message))
