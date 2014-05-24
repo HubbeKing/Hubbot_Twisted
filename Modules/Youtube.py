b2a147414f7c62a55032959cafef903ccbc68c8d
HubbeKing <hubbe128@gmail.com>
2014-05-24 23:08:16 +0300

    [Youtube] It works? IT WORKS NOW WE DRINK

--- Modules/Youtube.py
+++ Modules/Youtube.py
@@ -11,7 +11,7 @@ class Youtube(ModuleInterface):
         if message.Command == "randomyt":
             if len(message.ParameterList) == 0:
                 return IRCResponse(ResponseType.Say, "You didn't specify a channel!", message.ReplyTo)
-            else:
+            try:
                 author = " ".join(message.ParameterList)
                 feed = urllib.urlopen(r'https://gdata.youtube.com/feeds/api/users/{}?alt=json&v=2'.format(author))
                 numberOfVids = json.load(feed)
@@ -25,7 +25,9 @@ class Youtube(ModuleInterface):
                 inp.close()
                 returnVid = random.choice(resp['feed']['entry'])
                 return IRCResponse(ResponseType.Say, "{} -- {}".format(returnVid['title']['$t'], returnVid['link'][0]['href'].split("&",1)[0]), message.ReplyTo)
-
+            except Exception, ex:
+                print ex.args
+                return IRCResponse(ResponseType.Say, "Something broke.", message.ReplyTo)
         if message.Command == "latestyt":
             if len(message.ParameterList) == 0:
                 return IRCResponse(ResponseType.Say, "You didn't specify a channel!", message.ReplyTo)
