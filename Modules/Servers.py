4960f2a844fed43c92b6df112a52eadb87221fea
HubbeKing <hubbe128@gmail.com>
2014-08-17 18:33:10 +0300

    [Servers] Tweak Minecraft info

--- Modules/Servers.py
+++ Modules/Servers.py
@@ -20,7 +20,7 @@ class Servers(ModuleInterface):
                 "cockatrice":"The Cockatrice server is hosted at: cockatrice.dahou.se:4747",
                 "kf":"The Killing Floor server is hosted at: kf.dahou.se",
                 "tf2":"The Team Fortress 2 server is hosted at tf2.dahou.se",
-                "minecraft":"The FTB Monster server is hosted at craft.dahou.se (PM MasterCheese for whitelisting)",
+                "minecraft":"The FTB Monster server is hosted at craft.dahou.se (PM MasterCheese for build permission)",
                 "<server>":"Seriously?"
             }
         self.triggers.extend(self.serverDict.keys())
