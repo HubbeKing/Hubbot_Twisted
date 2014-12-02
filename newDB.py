bd85733588bd1de1b8c5e9d3f6b513803ad02d3e
Hubbe King <hubbe128@gmail.com>
2014-12-02 12:54:01 +0200

    [Misc] Fix some small folder-related things

--- newDB.py
+++ newDB.py
@@ -3,7 +3,7 @@ import sqlite3
 hugs = ["hubbeking",0,0]
 line = "Symphony is a cyborg."
 
-conn = sqlite3.connect("data/data.db")
+conn = sqlite3.connect("hubbot/data/data.db")
 c = conn.cursor()
 
 c.execute("CREATE TABLE hugs (nick text, given int, received int)")
