a8d880fb9158a0739519152febb1e0b02bb34d8c
Hubbe King <hubbe128@gmail.com>
2014-12-04 12:12:30 +0200

    [Alias] Allow aliases of aliases
    Also fancy the module up a bit.

--- hubbot/Modules/Alias.py
+++ hubbot/Modules/Alias.py
@@ -1,3 +1,4 @@
+import re
 import sqlite3
 from hubbot.moduleinterface import ModuleInterface
 from hubbot.response import IRCResponse, ResponseType
@@ -36,22 +37,18 @@ class Alias(ModuleInterface):
                 if len(message.ParameterList) <= 1:
                     return IRCResponse(ResponseType.Say, "Alias what?", message.ReplyTo)
 
-                triggerFound = False
-                for (name, module) in self.bot.moduleHandler.modules.items():
-                    if message.ParameterList[0] in module.triggers:
-                        return IRCResponse(ResponseType.Say, "'{}' is already a command!".format(message.ParameterList[0]), message.ReplyTo)
-                    if message.ParameterList[1] in module.triggers:
-                        triggerFound = True
+                if message.ParameterList[0] in self.bot.moduleHandler.mappedTriggers:
+                    return IRCResponse(ResponseType.Say, "'{}' is already a command!".format(message.ParameterList[0]), message.ReplyTo)
 
-                if not triggerFound:
-                    return IRCResponse(ResponseType.Say, "'{}' is not a valid command!".format(message.ParameterList[1]), message.ReplyTo)
+                if message.ParameterList[1] not in self.bot.moduleHandler.mappedTriggers and message.ParameterList[1] not in self.aliases:
+                    return IRCResponse(ResponseType.Say, "'{}' is not a valid command or alias!".format(message.ParameterList[1]), message.ReplyTo)
                 if message.ParameterList[0] in self.aliases.keys():
                     return IRCResponse(ResponseType.Say, "'{}' is already an alias!".format(message.ParameterList[0]), message.ReplyTo)
 
                 aliasParams = []
                 for word in message.ParameterList[1:]:
                     aliasParams.append(word)
-                self.newAlias(message.ParameterList[0], aliasParams)
+                self._newAlias(message.ParameterList[0], aliasParams)
 
                 return IRCResponse(ResponseType.Say, "Created a new alias '{}' for '{}'.".format(message.ParameterList[0], " ".join(message.ParameterList[1:])), message.ReplyTo)
             elif message.Command == "unalias":
@@ -62,29 +59,35 @@ class Alias(ModuleInterface):
                     return IRCResponse(ResponseType.Say, "Unalias what?", message.ReplyTo)
 
                 if message.ParameterList[0] in self.aliases.keys():
-                    self.deleteAlias(message.ParameterList[0])
+                    self._deleteAlias(message.ParameterList[0])
                     return IRCResponse(ResponseType.Say, "Deleted alias '{}'".format(message.ParameterList[0]), message.ReplyTo)
                 else:
                     return IRCResponse(ResponseType.Say, "I don't have an alias '{}'".format(message.ParameterList[0]), message.ReplyTo)
 
         elif message.Command in self.aliases.keys():
-            self.bot.moduleHandler.handleMessage(self.aliasedMessage(message))
+            newMessage = self._aliasedMessage(message)
+            newCommand = newMessage.Command
+            if newCommand in self.bot.moduleHandler.mappedTriggers:
+                return self.bot.moduleHandler.mappedTriggers[newCommand].onTrigger(newMessage)
+            elif newCommand in self.aliases:
+                newMessage = self._aliasedMessage(message)
+                return self.onTrigger(newMessage)
 
-    def newAlias(self, alias, command):
+    def _newAlias(self, alias, command):
         self.aliases[alias] = command
         with sqlite3.connect(self.bot.databaseFile) as conn:
             c = conn.cursor()
             c.execute("INSERT INTO aliases VALUES (?,?)", (alias, " ".join(command)))
             conn.commit()
 
-    def deleteAlias(self, alias):
+    def _deleteAlias(self, alias):
         del self.aliases[alias]
         with sqlite3.connect(self.bot.databaseFile) as conn:
             c = conn.cursor()
             c.execute("DELETE FROM aliases WHERE alias=?", (alias,))
             conn.commit()
 
-    def aliasedMessage(self, message):
+    def _aliasedMessage(self, message):
         """
         @type message: hubbot.message.IRCMessage
         """
@@ -93,16 +96,16 @@ class Alias(ModuleInterface):
             newMsg = message.MessageString.replace(message.Command, " ".join(alias), 1)
             if "$sender" in newMsg:
                 newMsg = newMsg.replace("$sender", message.User.Name)
-            if "$channel" in newMsg:
+            if "$channel" in newMsg and message.ChannelObj is not None:
                 newMsg = newMsg.replace("$channel", message.ChannelObj.Name)
-            if "$0" in newMsg:
-                newMsg = newMsg.replace(message.Parameters, "")
-                newMsg = newMsg.replace("$0", " ".join(message.ParameterList))
-            if len(message.ParameterList) >= 1 and "$" in newMsg:
-                newMsg = newMsg.replace(message.Parameters, "")
+
+            if re.search(r'\$[0-9]+', newMsg):  # if the alias contains numbered param replacement points, replace them
+                newMsg = newMsg.replace('$0',  u' '.join(message.ParameterList))
                 for i, param in enumerate(message.ParameterList):
-                    if newMsg.find("${}+".format(i+1)) != -1:
-                        newMsg = newMsg.replace("${}+".format(i+1), " ".join(message.ParameterList[i:]))
+                    if newMsg.find(u"${}+".format(i+1)) != -1:
+                        newMsg = newMsg.replace(u"${}+".format(i+1), u" ".join(message.ParameterList[i:]))
                     else:
-                        newMsg = newMsg.replace("${}".format(i+1), param)
+                        newMsg = newMsg.replace(u"${}".format(i+1), param)
+            else:  # if there are no numbered replacement points, append the full parameter list instead
+                newMsg += u' {}'.format(u' '.join(message.ParameterList))
             return IRCMessage(message.Type, message.User.String, message.ChannelObj, newMsg, self.bot)
