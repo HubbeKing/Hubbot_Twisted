173348372c14e6b95c21da200224c8e9b8eb8b86
Hubbe King <hubbe128@gmail.com>
2014-12-05 19:42:38 +0200

    [Triggers] Enables fetching of triggers per module

--- hubbot/Modules/Triggers.py
+++ hubbot/Modules/Triggers.py
@@ -4,14 +4,29 @@ from hubbot.moduleinterface import ModuleInterface
 
 class Triggers(ModuleInterface):
     triggers = ["triggers"]
-    help = "triggers -- returns a list of all command triggers, must be over PM"
+    help = "triggers [module] -- returns a list of all commands, if no module is specified, " \
+           "returns all commands currently loaded."
 
     def onTrigger(self, message):
         """
         @type message: hubbot.message.IRCMessage
         """
-        if message.User.Name != message.ReplyTo:
-            return IRCResponse(ResponseType.Say, "{} must be used over PM!".format(message.Command), message.ReplyTo)
+        if len(message.ParameterList) == 0:
+            if message.User.Name != message.ReplyTo:
+                return IRCResponse(ResponseType.Say, "{} must be used over PM!".format(message.Command),
+                                   message.ReplyTo)
+            else:
+                response = ", ".join(sorted(self.bot.moduleHandler.mappedTriggers.keys()))
+                return IRCResponse(ResponseType.Say, response, message.ReplyTo)
         else:
-            response = ", ".join(sorted(self.bot.moduleHandler.mappedTriggers.keys()))
-            return IRCResponse(ResponseType.Say, response, message.ReplyTo)
+            if message.ParameterList[0].lower() not in self.bot.moduleHandler.moduleCaseMapping:
+                return IRCResponse(ResponseType.Say, "No module named \"{}\" is currently loaded!", message.ReplyTo)
+            moduleToGet = self.bot.moduleHandler.moduleCaseMapping[message.ParameterList[0].lower()]
+            commands = []
+            for trigger, module in self.bot.moduleHandler.mappedTriggers.items():
+                if module == moduleToGet:
+                    commands.append(trigger)
+            return IRCResponse(ResponseType.Say,
+                               "Module \"{}\" contains the triggers: {}".format(message.ParameterList[0],
+                                                                                ", ".join(commands)),
+                               message.ReplyTo)
