376f814298614d6b86611ad66e9a29892ceef410
Hubbe King <hubbe128@gmail.com>
2014-12-08 17:46:50 +0200

    [Silly] Code cleanup

--- hubbot/Modules/Silly.py
+++ hubbot/Modules/Silly.py
@@ -3,46 +3,43 @@ from hubbot.moduleinterface import ModuleInterface
 
 
 class Silly(ModuleInterface):
+    sillyDict = {
+        "silly": "",
+        "both": "http://www.youtube.com/watch?v=OawrlVoQqSs",
+        "disappointed": "https://31.media.tumblr.com/cea6574a24b490ada8bec694e87b307b/tumblr_n3blu9k5CW1tsipf6o6_400.gif",
+        "donotwant": "http://www.youtube.com/watch?v=oKI-tD0L18A",
+        "fixyou": "http://www.youtube.com/watch?v=oin0KNElSG0",
+        "goat": "http://www.youtube.com/watch?v=SIaFtAKnqBU",
+        "heya": "http://www.youtube.com/watch?v=ZZ5LpwO-An4",
+        "hngh": "http://24.media.tumblr.com/tumblr_malzltlKk21rggqwfo1_500.gif",
+        "hunt": "LAPTOP CHAT ENGAGE\nTHRUSTERS TO MAXIMUM POWER\nFLOOD TUBES THREE AND FOUR\nHUNT FOR RED OCTOBER",
+        "mybrand": "http://www.youtube.com/watch?v=V-fRuoMIfpw",
+        "nope": "http://www.youtube.com/watch?v=gvdf5n-zI14",
+        "psc": "https://docs.google.com/document/d/1C4Maba3fXrLYMMIXK67OOn7C2c31V6nff5inGAZStsw/edit?pli=1",
+        "store": "http://www.youtube.com/watch?v=iRZ2Sh5-XuM",
+        "stupid": "http://www.youtube.com/watch?v=yytbDZrw1jc",
+        "yes": "http://www.youtube.com/watch?v=P3ALwKeSEYs",
+        "yup": "http://www.youtube.com/watch?v=K0QHw7iy1Rg",
+        "what": "http://www.youtube.com/watch?v=PDXrXBsTFSE",
+        "whoa": "http://www.youtube.com/watch?v=6q-gHrEaEAs"
+    }
 
     def onLoad(self):
-        self.help = "donotwant, yes, yup, store, goat, stupid, fixyou, heya, HNGH, PS, whoa, what, mybrand, both, disappointed -- Used to post silly things! Usage: {}<thing>".format(self.bot.CommandChar)
-        self.linkDict = \
-            {
-                "silly":"",
-                "nope":"http://www.youtube.com/watch?v=gvdf5n-zI14",
-                "donotwant":"http://www.youtube.com/watch?v=oKI-tD0L18A",
-                "yes":"http://www.youtube.com/watch?v=P3ALwKeSEYs",
-                "store":"http://www.youtube.com/watch?v=iRZ2Sh5-XuM",
-                "stupid":"http://www.youtube.com/watch?v=yytbDZrw1jc",
-                "fixyou":"http://www.youtube.com/watch?v=oin0KNElSG0",
-                "yup":"http://www.youtube.com/watch?v=K0QHw7iy1Rg",
-                "heya":"http://www.youtube.com/watch?v=ZZ5LpwO-An4",
-                "what":"http://www.youtube.com/watch?v=PDXrXBsTFSE",
-                "goat":"http://www.youtube.com/watch?v=SIaFtAKnqBU",
-                "mybrand":"http://www.youtube.com/watch?v=V-fRuoMIfpw",
-                "whoa":"http://www.youtube.com/watch?v=6q-gHrEaEAs",
-                "hngh":"http://24.media.tumblr.com/tumblr_malzltlKk21rggqwfo1_500.gif",
-                "ps":"https://docs.google.com/document/d/1C4Maba3fXrLYMMIXK67OOn7C2c31V6nff5inGAZStsw/edit?pli=1",
-                "psc":"https://docs.google.com/document/d/1C4Maba3fXrLYMMIXK67OOn7C2c31V6nff5inGAZStsw/edit?pli=1",
-                "both":"http://www.youtube.com/watch?v=OawrlVoQqSs",
-                "hunt":"",
-                "<thing>":"Har Har.",
-                "disappointed":"https://31.media.tumblr.com/cea6574a24b490ada8bec694e87b307b/tumblr_n3blu9k5CW1tsipf6o6_400.gif"
-            }
-        self.triggers = self.linkDict.keys()
+        self.triggers = self.sillyDict.keys()
+        sillyList = [item for item in self.triggers if item != "silly"]
+        self.help = "{} -- Used to post silly things!".format(", ".join(sorted(sillyList)))
+
+    def help(self, message):
+        command = message.ParameterList[0].lower()
+        if command != "hunt":
+            return self.sillyDict[command]
 
     def onTrigger(self, message):
         """
         @type message: hubbot.message.IRCMessage
         """
-        if message.Command == "silly":
-            return IRCResponse(ResponseType.Say, self.help, message.ReplyTo)
-        elif message.Command == "hunt":
+        if message.Command == "hunt":
             if message.User.Name in self.bot.admins:
-                line1 = "LAPTOP CHAT ENGAGE"
-                line2 = "THRUSTERS TO MAXIMUM POWER"
-                line3 = "FLOOD TUBES THREE AND FOUR"
-                line4 = "HUNT FOR RED OCTOBER"
-                return IRCResponse(ResponseType.Say, line1, message.ReplyTo), IRCResponse(ResponseType.Say, line2, message.ReplyTo), IRCResponse(ResponseType.Say, line3, message.ReplyTo), IRCResponse(ResponseType.Say, line4, message.ReplyTo)
+                return IRCResponse(ResponseType.Say, self.sillyDict["hunt"], message.ReplyTo)
         else:
-            return IRCResponse(ResponseType.Say, self.linkDict[message.Command], message.ReplyTo)
+            return IRCResponse(ResponseType.Say, self.sillyDict[message.Command], message.ReplyTo)
