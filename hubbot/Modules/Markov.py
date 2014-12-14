1b6f35f3e437278750150fde030202f4b0a8d909
Hubbe King <hubbe128@gmail.com>
2014-12-14 14:37:35 +0200

    [Markov] Use COBE for better chains

--- hubbot/Modules/Markov.py
+++ hubbot/Modules/Markov.py
@@ -1,56 +1,23 @@
 from hubbot.moduleinterface import ModuleInterface
 from hubbot.response import IRCResponse, ResponseType
-from collections import defaultdict
+from cobe.brain import Brain
 import os
-import random
 
 
 class Markov(ModuleInterface):
-    markov = defaultdict(list)
-    STOP_WORD = "\n"
-    chattiness = 0.05
-    chainLength = 2
-    maxWords = 100
     help = "Markov - Yeah I'm sentient, what of it?"
 
-    def addToBrain(self, msg, write=True):
-        if "://" in msg or len(msg) <= 1:
-            return
-        if write:
-            with open(os.path.join("hubbot", "data", "{}.brain".format(self.bot.server)), "a") as brainFile:
-                brainFile.write(msg + "\n")
-        buf = [self.STOP_WORD] * self.chainLength
-        for word in msg.split():
-            self.markov[tuple(buf)].append(word)
-            del buf[0]
-            buf.append(word)
-        self.markov[tuple(buf)].append(self.STOP_WORD)
-
-    def generateSentence(self, msg):
-        buf = msg.split()[:self.chainLength]
-        if len(msg.split()) > self.chainLength:
-            message = buf[:]
-        else:
-            message = []
-            for i in xrange(self.chainLength):
-                message.append(random.choice(self.markov[random.choice(self.markov.keys())]))
-        for i in xrange(self.maxWords):
-            try:
-                nextWord = random.choice(self.markov[tuple(buf)])
-            except IndexError:
-                continue
-            if nextWord == self.STOP_WORD:
-                break
-            message.append(nextWord)
-            del buf[0]
-            buf.append(nextWord)
-        return " ".join(message)
-
     def onLoad(self):
-        if os.path.exists(os.path.join("hubbot", "data", "{}.brain".format(self.bot.server))):
-            with open(os.path.join("hubbot", "data", "{}.brain".format(self.bot.server)), "r") as brainFile:
-                for line in brainFile:
-                    self.addToBrain(line, write=False)
+        self.brain = Brain(os.path.join("hubbot", "data", "{}.brain".format(self.bot.server)))
+
+    def onUnload(self):
+        for channel in self.bot.channels:
+            response = IRCResponse(ResponseType.Say, "Fine, I'll shut up.", channel.Name)
+            self.bot.moduleHandler.sendResponse(response)
+
+    def addToBrain(self, msg):
+        if "://" not in msg and len(msg) > 1:
+            self.brain.learn(msg)
 
     def shouldTrigger(self, message):
         return True
@@ -61,13 +28,8 @@ class Markov(ModuleInterface):
         """
         if message.User is None or message.Channel is None:
             return
-        if message.MessageList[0].startswith(self.bot.nickname):
-            msg = " ".join(message.MessageList[1:])
-            self.addToBrain(msg)
-            if random.random() <= self.chattiness:
-                sentence = self.generateSentence(msg)
-                if sentence:
-                    return IRCResponse(ResponseType.Say, sentence, message.ReplyTo)
+        if message.MessageList[0].startswith(self.bot.nickname) and len(message.MessageList) > 1:
+            reply = self.brain.reply(" ".join(message.MessageList[1:]), max_len=50)
+            return IRCResponse(ResponseType.Say, reply.capitalize(), message.ReplyTo)
         else:
-            msg = " ".join(message.MessageList)
-            self.addToBrain(msg)
+            self.addToBrain(" ".join(message.MessageList))
