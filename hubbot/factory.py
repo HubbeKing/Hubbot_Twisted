08f103ecf371599fd6b75df30029160ce471cb75
HubbeKing <hubbe128@gmail.com>
2014-12-29 20:03:29 +0200

    [Core] More logging, requirements fix?

--- hubbot/factory.py
+++ hubbot/factory.py
@@ -13,20 +13,20 @@ class HubbotFactory(protocol.ReconnectingClientFactory):
         reactor.connectTCP(server, port, self)
 
     def startedConnecting(self, connector):
-        print "-#- Started to connect to '{}'.".format(self.bot.server)
+        self.bot.logger.info("-#- Started to connect to '{}'.".format(self.bot.server))
 
     def buildProtocol(self, addr):
-        print "-#- Connected to '{}'.".format(self.bot.server)
-        print "-#- Resetting reconnection delay."
+        self.bot.logger.info("-#- Connected to '{}'.".format(self.bot.server))
+        self.bot.logger.info("-#- Resetting reconnection delay.")
         self.resetDelay()
         return self.bot
 
     def clientConnectionLost(self, connector, reason):
         if not self.bot.Quitting:
-            print "-!- Connection lost. Reason:", reason
+            self.bot.logger.info("-!- Connection lost. Reason:", reason)
             protocol.ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
 
     def clientConnectionFailed(self, connector, reason):
-        print "-!- Connection failed. Reason:", reason
+        self.bot.logger.info("-!- Connection failed. Reason:", reason)
         protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
