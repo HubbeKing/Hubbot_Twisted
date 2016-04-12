f6d16b8ae0b394180077bb027525702635a7247e
HubbeKing <hubbe128@gmail.com>
2016-04-12 21:30:15 +0300

    [Notify] Write a module to notify me via pushbullet if I'm highlighted

---
+++ hubbot/Modules/Notify.py
@@ -0,0 +1,66 @@
+import re
+import sqlite3
+from hubbot.moduleinterface import ModuleInterface, ModuleAccessLevel
+from pushbullet import PushBullet, InvalidKeyError
+
+
+class Notify(ModuleInterface):
+    accessLevel = ModuleAccessLevel.ADMINS
+    pb = None
+    APIKey = None
+    regexPattern = "Hubbe.*"
+
+    def getAPIkey(self):
+        """
+        Get the API key for pushbullet from the sqlite database.
+        """
+        apiKey = None
+        with sqlite3.connect(self.bot.databaseFile) as conn:
+            c = conn.cursor()
+            for row in c.execute("SELECT apikey FROM keys WHERE name=\"pushbullet\""):
+                apiKey = row[0]
+        return apiKey
+
+    def onEnable(self):
+        """
+        When the module is enabled, try to get the API key for Pushbullet and authenticate with it.
+        """
+        try:
+            self.APIKey = self.getAPIkey()
+            self.pb = None
+            if self.APIKey is not None:
+                try:
+                    self.pb = PushBullet(self.APIKey)
+                except InvalidKeyError:
+                    self.bot.logger.exception("Pushbullet API key invalid!")
+                    raise
+                self.bot.logger.info("Successfully authenticated with Pushbullet API key.")
+            else:
+                self.bot.logger.error("Could not find Pushbullet API key!")
+        except:
+            self.bot.logger.error("Error when fetching Pushbullet API key!")
+            raise
+
+    def shouldTrigger(self, message):
+        """
+        @type message: hubbot.message.IRCMessage
+        """
+        if message.Type in self.acceptedTypes:
+            match = re.search(self.regexPattern, message.MessageString, re.IGNORECASE)
+            if match:
+                return True
+            else:
+                return False
+
+    def onTrigger(self, message):
+        """
+        @type message: hubbot.message.IRCMessage
+        """
+        try:
+            self.pb.refresh()
+            push = self.pb.push_note("Highlight in {}".format(message.Channel.Name), message.MessageString)
+        except:
+            self.bot.logger.exception("Highlighting pushbullet push failed.")
+        else:
+            if "error" in push:
+                self.bot.logger.error("Pushbullet returned error '{}'".format(push["error"]["type"]))
