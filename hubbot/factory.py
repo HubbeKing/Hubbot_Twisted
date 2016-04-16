9559e7d4d6232a4914712eb86617e74e38e7f149
HubbeKing <hubbe128@gmail.com>
2016-04-16 14:35:18 +0300

    [Core] THROW MORE STUFF

--- hubbot/factory.py
+++ hubbot/factory.py
@@ -18,10 +18,10 @@ class HubbotFactory(protocol.ReconnectingClientFactory):
         """
         @type connector: twisted.internet.tcp.Connector
         """
-        self.bot.logger.info("-#- Started to connect to \"{}\".".format(connector.bindAddress))
+        self.bot.logger.info("-#- Started to connect to \"{}\".".format(self.transport.getPeer().host))
 
     def buildProtocol(self, addr):
-        self.bot.logger.info("-#- Connected.")
+        self.bot.logger.info("-#- Connected to \"{}\".".format(self.bot.server))
         self.bot.logger.info("-#- Resetting reconnection delay.")
         self.resetDelay()
         return self.bot
@@ -32,7 +32,7 @@ class HubbotFactory(protocol.ReconnectingClientFactory):
         @type reason: twisted.python.failure.Failure
         """
         if not self.bot.Quitting:
-            self.bot.logger.warning("-!- Connection to \"{}\" lost, reason: \"{}\" Retrying in {} seconds.".format(connector.bindAddress, reason.getErrorMessage(), self.delay))
+            self.bot.logger.warning("-!- Connection to \"{}\" lost, reason: \"{}\" Retrying in {} seconds.".format(self.transport.getPeer().host, reason.getErrorMessage(), self.delay))
             protocol.ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
 
     def clientConnectionFailed(self, connector, reason):
@@ -40,5 +40,5 @@ class HubbotFactory(protocol.ReconnectingClientFactory):
         @type connector: twisted.internet.tcp.Connector
         @type reason: twisted.python.failure.Failure
         """
-        self.bot.logger.warning("-!- Connection to \"{}\" failed, reason: \"{}\" Retrying in {} seconds.".format(connector.bindAddress, reason.getErrorMessage(), self.delay))
+        self.bot.logger.warning("-!- Connection to \"{}\" failed, reason: \"{}\" Retrying in {} seconds.".format(self.transport.getPeer().host, reason.getErrorMessage(), self.delay))
         protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
