cef978cca6711e0b7c686a3bc0fbc579aa611e60
HubbeKing <hubbe128@gmail.com>
2016-12-13 17:26:20 +0200

    [Uptime] Refactoring stuff is a butt
    PyCharm why are you so bad at this

--- hubbot/Modules/Uptime.py
+++ hubbot/Modules/Uptime.py
@@ -13,5 +13,5 @@ class Uptime(ModuleInterface):
         @type message: hubbot.message.IRCMessage
         """
         now = datetime.datetime.utcnow()
-        time_delta = now - self.bot.startTime
+        time_delta = now - self.bot.start_time
         return IRCResponse(ResponseType.SAY, "I have been running for {}!".format(delta_time_to_string(time_delta, resolution="s")), message.reply_to)
