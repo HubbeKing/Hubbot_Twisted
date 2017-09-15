c7436bee3959109bc62be792630a5e59f06d12e9
HubbeKing <HubbeKing@users.noreply.github.com>
2017-09-15 23:36:00 +0300

    Update Hugs module
    Use proper SQL queries rather than building dicts
    Add a consolidation function for "migrating" hugs

--- hubbot/Modules/Hugs.py
+++ hubbot/Modules/Hugs.py
@@ -9,10 +9,10 @@ except ImportError:
 
 
 class Hugs(ModuleInterface):
-    triggers = ["hugs"]
+    triggers = ["hugs", "consolidatehugs"]
     accepted_types = ["PRIVMSG", "ACTION"]
     help = "hugs [nick] -- How many hugs has this person given and received?"
-    # hug_dict is : {"nick":[given, received]}
+    # hugs table is nick; given; received
     common_words = [
         "and", "of", "all", "to", "the", "both", "back", "again",
         "any", "one", "<3", "with", "", "<3s", "so", "hard", "right",
@@ -41,6 +41,21 @@ class Hugs(ModuleInterface):
             c.execute("CREATE TABLE IF NOT EXISTS hugs (nick text, given int, received int)")
             conn.commit()
 
+    def consolidate_hugs(self, from_nick, to_nick):
+        with sqlite3.connect(self.bot.database_file) as conn:
+            temp_dict = {}
+            c = conn.cursor()
+            c.execute("SELECT * FROM hugs WHERE nick LIKE ?", (from_nick,))
+            for row in c.fetchall():
+                temp_dict[row[0]] = [row[1], row[2]]
+            consolidated_hugs_count = [0, 0]
+            for nick, hug_list in temp_dict.iteritems():
+                c.execute("DELETE FROM hugs WHERE nick=?", (nick,))
+                consolidated_hugs_count[0] += hug_list[0]
+                consolidated_hugs_count[1] += hug_list[1]
+            c.execute("INSERT INTO hugs VALUES (?,?,?)", (to_nick, consolidated_hugs_count[0], consolidated_hugs_count[1]))
+            conn.commit()
+
     def on_trigger(self, message):
         """
         @type message: hubbot.message.IRCMessage
@@ -50,11 +65,6 @@ class Hugs(ModuleInterface):
             match = re.search(pattern, message.message_list[0], re.IGNORECASE)
             if match:
                 self.bot.logger.info('{} *{} {}*'.format(message.reply_to, message.user.name, message.message_string))
-                hug_dict = {}
-                with sqlite3.connect(self.bot.database_file) as conn:
-                    c = conn.cursor()
-                    for row in c.execute("SELECT * FROM hugs"):
-                        hug_dict[row[0]] = [row[1], row[2]]
                 receivers = []
                 for nick in message.message_list[1:]:
                     if nick.lower() not in self.common_words:
@@ -70,39 +80,24 @@ class Hugs(ModuleInterface):
                 for index in range(0, len(receivers)):
                     giver = message.user.name.lower()
                     receiver = receivers[index]
-                    if giver not in hug_dict:
-                        hug_dict[giver] = [1, 0]
-                        with sqlite3.connect(self.bot.database_file) as conn:
-                            c = conn.cursor()
+                    with sqlite3.connect(self.bot.database_file) as conn:
+                        c = conn.cursor()
+                        c.execute("SELECT count(*) FROM hugs WHERE nick=?", (giver,))
+                        giver_exists = c.fetchone()[0]
+                        if giver_exists == 0:
+                            # Giver not in database
                             c.execute("INSERT INTO hugs VALUES (?,?,?)", (giver, 1, 0))
-                            conn.commit()
-                    else:
-                        hug_dict[giver] = list(hug_dict[giver])
-                        hug_dict[giver][0] += 1
-                        with sqlite3.connect(self.bot.database_file) as conn:
-                            c = conn.cursor()
-                            c.execute("UPDATE hugs SET given=? WHERE nick=?", (hug_dict[giver][0], giver))
-                            conn.commit()
-                    if receiver not in hug_dict:
-                        hug_dict[receiver] = [0, 1]
-                        with sqlite3.connect(self.bot.database_file) as conn:
-                            c = conn.cursor()
+                        else:
+                            c.execute("UPDATE hugs SET given = given + 1 WHERE nick=?", (giver,))
+                        c.execute("SELECT count(*) FROM hugs WHER nick=?", (receiver,))
+                        receiver_exists = c.fetchone()[0]
+                        if receiver_exists == 0:
                             c.execute("INSERT INTO hugs VALUES (?,?,?)", (receiver, 0, 1))
-                            conn.commit()
-                    else:
-                        hug_dict[receiver] = list(hug_dict[receiver])
-                        hug_dict[receiver][1] += 1
-                        with sqlite3.connect(self.bot.database_file) as conn:
-                            c = conn.cursor()
-                            c.execute("UPDATE hugs SET received=? WHERE nick=?", (hug_dict[receiver][1], receiver))
-                            conn.commit()
+                        else:
+                            c.execute("UPDATE hugs SET received = received + 1 WHERE nick=?", (receiver,))
+                        conn.commit()
 
-        elif message.type == "PRIVMSG":
-            hug_dict = {}
-            with sqlite3.connect(self.bot.database_file) as conn:
-                c = conn.cursor()
-                for row in c.execute("SELECT * FROM hugs"):
-                    hug_dict[row[0]] = [row[1], row[2]]
+        elif message.type == "PRIVMSG" and message.command == "hugs":
             if len(message.parameter_list) == 0:
                 target = message.user.name
             else:
@@ -110,18 +105,19 @@ class Hugs(ModuleInterface):
 
             hug_data = [0, 0]
             matches = []
-            for (user, hugCounts) in hug_dict.items():
-                try:
-                    match = re.search(target, user, re.IGNORECASE)
-                except Exception:
-                    match = False
-                if match:
-                    matches.append(match.string)
-                    hug_data[0] += hugCounts[0]
-                    hug_data[1] += hugCounts[1]
+
+            with sqlite3.connect(self.bot.database_file) as conn:
+                wrapped_target = "%{}%".format(target)
+                c = conn.cursor()
+                c.execute("SELECT * FROM hugs WHERE nick LIKE ?", (wrapped_target,))
+
+                for row in c.fetchall():
+                    matches.append(row[0])
+                    hug_data[0] += row[1]
+                    hug_data[1] += row[2]
 
             hug_string = "{} has received {} hugs and given {} hugs.".format(target, hug_data[1], hug_data[0])
-            matched_nicks_string = "Matches found: "
+            matched_nicks_string = "Matches found with '{}': ".format(wrapped_target)
             number_of_matches = 0
             for name in matches:
                 if number_of_matches > 9:
@@ -134,7 +130,23 @@ class Hugs(ModuleInterface):
                     matched_nicks_string = matched_nicks_string + name + ", "
                     number_of_matches += 1
             if len(matches) > 30:
-                matched_nicks_string = "Matches found: LOTS."
+                matched_nicks_string = "Matches found with '{}': LOTS.".format(wrapped_target)
             return IRCResponse(ResponseType.SAY, matched_nicks_string, message.reply_to), IRCResponse(ResponseType.SAY,
                                                                                                       hug_string,
                                                                                                       message.reply_to)
+        elif message.type == "PRIVMSG" and message.command == "consolidatehugs":
+            if message.user.name in self.bot.admins:
+                from_nick = message.parameter_list[0]
+                to_nick = message.parameter_list[1]
+                self.consolidate_hugs(from_nick, to_nick)
+
+                wrapped_to_nick = "%{}%".format(to_nick)
+                hug_data = [0, 0]
+                with sqlite3.connect(self.bot.database_file) as conn:
+                    c = conn.cursor()
+                    c.execute("SELECT * FROM hugs WHERE nick LIKE ?", (wrapped_to_nick,))
+                    for row in c.fetchall():
+                        hug_data[0] += row[1]
+                        hug_data[1] += row[2]
+                return IRCResponse(ResponseType.SAY, "Success, probably.", message.reply_to), \
+                       IRCResponse(ResponseType.SAY, "{} now has received {} hugs and given {} hugs.".format(to_nick, hug_data[1], hug_data[0]), message.reply_to)
