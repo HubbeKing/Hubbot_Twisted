d73d9bc4064406b6b52473f61fdc0dbc97713b89
HubbeKing <HubbeKing@users.noreply.github.com>
2018-10-12 13:17:51 +0300

    Basic module for unmasking hostname

---
+++ hubbot/Modules/Unmask.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+from hubbot.moduleinterface import ModuleInterface, ModuleAccessLevel
+from hubbot.response import IRCResponse, ResponseType
+
+
+class Unmask(ModuleInterface):
+    triggers = ["unmask"]
+    help = "unmask - sends a MODE -x command to unmask hostname on some ircds"
+    access_level = ModuleAccessLevel.ADMINS
+    
+    def on_trigger(self, message):
+        """
+        @type message: hubbot.message.IRCMessage
+        """
+        return IRCResponse(ResponseType.RAW, "MODE {} -x".format(self.bot.nickname), message.replyTo)
